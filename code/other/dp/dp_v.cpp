9572eb5a067b8c4a8fb34e10d8006548340665c0
GitHub <noreply@github.com>
2022-06-18 23:21:57 +0000

    [auto-commit] Add my codes

--- code/other/dp/dp_v.cpp
+++ code/other/dp/dp_v.cpp
@@ -145,183 +145,6 @@ struct Graph<void> {
     int _size;
     std::vector<std::vector<edge_type>> edges;
 };
-#line 3 "/home/kuhaku/home/github/algo/lib/math/modint.hpp"
-
-/**
- * @brief modint
- * @see https://github.com/ei1333/library/blob/master/math/combinatorics/mod-int.cpp
- *
- * @tparam mod 法
- */
-template <int mod = MOD_N>
-struct ModInt {
-    static constexpr int get_mod() noexcept { return mod; }
-
-    constexpr ModInt() noexcept : x(0) {}
-    constexpr ModInt(int y) noexcept : x(y >= 0 ? y % mod : (mod - 1 - ~y % mod)) {}
-    constexpr ModInt(std::int64_t y) noexcept : x(y >= 0 ? y % mod : (mod - 1 - ~y % mod)) {}
-
-    constexpr ModInt &operator+=(const ModInt &rhs) noexcept {
-        if ((this->x += rhs.x) >= mod) this->x -= mod;
-        return *this;
-    }
-    constexpr ModInt &operator-=(const ModInt &rhs) noexcept {
-        if ((this->x += mod - rhs.x) >= mod) this->x -= mod;
-        return *this;
-    }
-    constexpr ModInt &operator*=(const ModInt &rhs) noexcept {
-        this->x = (int)(1LL * this->x * rhs.x % mod);
-        return *this;
-    }
-    constexpr ModInt &operator/=(const ModInt &rhs) noexcept {
-        *this *= rhs.inverse();
-        return *this;
-    }
-
-    constexpr ModInt &operator++() noexcept {
-        if ((++(this->x)) >= mod) this->x -= mod;
-        return *this;
-    }
-    constexpr ModInt operator++(int) noexcept {
-        ModInt tmp(*this);
-        this->operator++();
-        return tmp;
-    }
-    constexpr ModInt &operator--() noexcept {
-        if ((this->x += mod - 1) >= mod) this->x -= mod;
-        return *this;
-    }
-    constexpr ModInt operator--(int) noexcept {
-        ModInt tmp(*this);
-        this->operator--();
-        return tmp;
-    }
-
-    constexpr ModInt operator-() const noexcept { return ModInt(-this->x); }
-    constexpr ModInt operator+(const ModInt &rhs) const noexcept { return ModInt(*this) += rhs; }
-    constexpr ModInt operator-(const ModInt &rhs) const noexcept { return ModInt(*this) -= rhs; }
-    constexpr ModInt operator*(const ModInt &rhs) const noexcept { return ModInt(*this) *= rhs; }
-    constexpr ModInt operator/(const ModInt &rhs) const noexcept { return ModInt(*this) /= rhs; }
-
-    constexpr bool operator==(const ModInt &rhs) const noexcept { return this->x == rhs.x; }
-    constexpr bool operator!=(const ModInt &rhs) const noexcept { return this->x != rhs.x; }
-
-    explicit operator int() const noexcept { return x; }
-
-    constexpr ModInt inverse() const noexcept {
-        int a = x, b = mod, u = 1, v = 0, t;
-        while (b > 0) {
-            t = a / b;
-            swap(a -= t * b, b);
-            swap(u -= t * v, v);
-        }
-        return ModInt(u);
-    }
-
-    constexpr ModInt pow(std::int64_t n) const noexcept { return ModInt(*this).pow_self(n); }
-    constexpr ModInt &pow_self(std::int64_t n) noexcept {
-        ModInt res(1);
-        for (; n > 0; n >>= 1) {
-            if (n & 1) res *= *this;
-            *this *= *this;
-        }
-        *this = res;
-        return *this;
-    }
-
-    friend std::istream &operator>>(std::istream &is, ModInt &rhs) {
-        std::int64_t t;
-        is >> t;
-        rhs = ModInt<mod>(t);
-        return (is);
-    }
-    friend std::ostream &operator<<(std::ostream &os, const ModInt &rhs) { return os << rhs.x; }
-
-  private:
-    int x;
-};
-#line 3 "/home/kuhaku/home/github/algo/lib/math/combination.hpp"
-
-template <int mod>
-struct Combination {
-    using mint = ModInt<mod>;
-
-    Combination() : _fact(), _finv() {}
-
-    mint operator()(int n, int k) {
-        if (n < k || n < 0 || k < 0) return 0;
-        this->_init(n);
-        return this->_fact[n] * this->_finv[k] * this->_finv[n - k];
-    }
-
-    mint fact(int x) {
-        assert(x >= 0);
-        this->_init(x);
-        return this->_fact[x];
-    }
-
-    mint finv(int x) {
-        assert(x >= 0);
-        this->_init(x);
-        return this->_finv[x];
-    }
-
-    mint naive(int n, int k) const {
-        if (n < k || n < 0 || k < 0) return 0;
-        if (n - k < k) k = n - k;
-        mint res = 1;
-        for (int i = 0; i < k; ++i) {
-            res *= n - i;
-            res /= i + 1;
-        }
-        return res;
-    }
-
-    mint lucas(int n, int k) {
-        if (n < k || n < 0 || k < 0) return 0;
-        if (n - k < k) k = n - k;
-        static vector<vector<mint>> v;
-        if (v.empty()) {
-            v = vector<vector<mint>>(mod, vector<mint>(mod));
-            for (int i = 0; i < mod; ++i) v[i][0] = 1;
-            for (int i = 0; i < mod; ++i) {
-                for (int j = 1; j < mod; ++j) {
-                    if (i < j) v[i][j] = 0;
-                    else if (i - j < j) v[i][j] = v[i][i - j];
-                    else v[i][j] = v[i][j - 1] * mint(i + 1 - j) / mint(j);
-                }
-            }
-        }
-        mint res = 1;
-        while (n || k) {
-            res *= v[n % mod][k % mod];
-            n /= mod, k /= mod;
-        }
-        return res;
-    }
-
-    mint permu(int n, int k) {
-        if (n < k || n < 0 || k < 0) return 0;
-        this->_init(n);
-        return this->_fact[n] * this->_finv[n - k];
-    }
-
-  private:
-    vector<mint> _fact, _finv;
-
-    void _init(int n) {
-        if (this->_fact.size() > n) return;
-        int m = this->_fact.size();
-        this->_fact.resize(n + 1);
-        for (int i = m; i <= n; ++i) {
-            if (i == 0) this->_fact[i] = 1;
-            else this->_fact[i] = this->_fact[i - 1] * i;
-        }
-        this->_finv.resize(n + 1);
-        this->_finv[n] = this->_fact[n].inverse();
-        for (int i = n - 1; i >= m; --i) this->_finv[i] = this->_finv[i + 1] * (i + 1);
-    }
-};
 #line 3 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
 using ll = int64_t;
 using ld = long double;
@@ -394,44 +217,29 @@ void Takahashi(bool is_correct = true) {
 void Aoki(bool is_not_correct = true) {
     Takahashi(!is_not_correct);
 }
-#line 5 "a.cpp"
+#line 4 "/home/kuhaku/home/github/algo/lib/tree/rerooting.hpp"
 
-Combination<MOD> combi;
-using Mint = ModInt<MOD>;
-struct Monoid {
-    using value_type = pair<int, Mint>;
-    static constexpr value_type id = {0, 1};
-    static value_type op(const value_type& lhs, const value_type& rhs) {
-        return {lhs.first + rhs.first,
-                lhs.second * rhs.second * combi(lhs.first + rhs.first, lhs.first)};
-    }
-
-    template <class T>
-    static constexpr value_type f(const value_type& v, T u) {
-        return v;
-    }
-
-    static value_type g(const value_type& v, int u) {
-        return {v.first + 1, v.second};
-    }
-};
-
+/**
+ * @brief 全方位木dp
+ * @see https://algo-logic.info/tree-dp/
+ *
+ * @tparam M モノイド
+ * @tparam T 辺の重みの型
+ */
 template <class M, class T>
 struct ReRooting {
   private:
     using Value = typename M::value_type;
 
   public:
-    ReRooting(const Graph<T>& g) : graph(g), dp(g.size()), data(g.size()) {
-        this->build();
-    }
+    ReRooting(const Graph<T> &g) : graph(g), dp(g.size()), data(g.size()) { this->build(); }
 
-    const Value& operator[](int i) const {
-        return data[i];
-    }
-    Value& operator[](int i) {
-        return data[i];
-    }
+    const auto &operator[](int i) const { return this->data[i]; }
+    auto &operator[](int i) { return this->data[i]; }
+    const auto begin() const { return this->data.begin(); }
+    auto begin() { return this->data.begin(); }
+    const auto end() const { return this->data.end(); }
+    auto end() { return this->data.end(); }
 
   private:
     Graph<T> graph;
@@ -449,8 +257,7 @@ struct ReRooting {
         dp[v] = std::vector<Value>(deg, M::id);
         for (int i = 0; i < deg; ++i) {
             auto e = graph[v][i];
-            if (e.to() == p)
-                continue;
+            if (e.to() == p) continue;
             dp[v][i] = M::f(dfs(e.to(), v), e.weight());
             res = M::op(res, dp[v][i]);
         }
@@ -461,30 +268,49 @@ struct ReRooting {
         std::vector<Value> dp_r(deg + 1, M::id);
         for (int i = deg - 1; i >= 0; --i) {
             auto e = graph[v][i];
-            if (e.to() == p)
-                dp[v][i] = M::f(dp_p, e.weight());
+            if (e.to() == p) dp[v][i] = M::f(dp_p, e.weight());
             dp_r[i] = M::op(dp[v][i], dp_r[i + 1]);
         }
         Value dp_l = M::id;
         for (int i = 0; i < deg; ++i) {
             int u = graph[v][i].to();
-            if (u != p)
-                this->bfs(u, v, M::g(M::op(dp_l, dp_r[i + 1]), u));
+            if (u != p) this->bfs(u, v, M::g(M::op(dp_l, dp_r[i + 1]), u));
             dp_l = M::op(dp_l, dp[v][i]);
         }
         data[v] = M::g(dp_l, v);
     }
 };
+#line 5 "a.cpp"
+
+struct Monoid {
+    using value_type = int;
+    static int m;
+    static constexpr value_type id = 1;
+    static value_type op(const value_type &lhs, const value_type &rhs) {
+        return (ll)lhs * rhs % m;
+    }
+
+    template <class T>
+    static constexpr value_type f(const value_type &v, T u) {
+        return v + 1;
+    }
+
+    static constexpr value_type g(const value_type &v, int u) {
+        return v;
+    }
+};
+
+int Monoid::m = 0;
 
 int main(void) {
     sonic();
     int n;
-    cin >> n;
+    cin >> n >> Monoid::m;
     Graph<void> g(n);
     g.input_edges(n - 1);
-    ReRooting<Monoid, void> re(g);
+    ReRooting<Monoid, void> rr(g);
 
-    rep(i, n) co(re[i].second);
+    rep(i, n) co(rr[i]);
 
     return 0;
 }

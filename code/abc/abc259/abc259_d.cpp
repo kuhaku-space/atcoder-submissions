33c104285e25b94782e8fb52ec0f8bf209cca04c
GitHub <noreply@github.com>
2022-07-09 23:23:17 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc259/abc259_d.cpp
@@ -0,0 +1,186 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using ll = int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 2 "/home/kuhaku/home/github/algo/lib/tree/union_find.hpp"
+
+/**
+ * @brief 素集合データ構造
+ * @details Implement (union by size) + (path compression)
+ * @see https://github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
+ */
+struct union_find {
+    union_find() : data() {}
+    union_find(int _n) : data(_n, -1) {}
+
+    int root(int x) { return this->data[x] < 0 ? x : this->data[x] = this->root(this->data[x]); }
+    int get_root(int x) { return this->root(x); }
+
+    bool is_root(int x) const { return this->data[x] < 0; }
+
+    bool same(int x, int y) { return this->root(x) == this->root(y); }
+    bool is_same(int x, int y) { return this->same(x, y); }
+
+    int size(int x) { return -(this->data[this->root(x)]); }
+    int get_size(int x) { return this->size(x); }
+
+    bool unite(int x, int y) {
+        x = this->root(x), y = this->root(y);
+        if (x == y) return false;
+        if (this->data[x] > this->data[y]) swap(x, y);
+        this->data[x] += this->data[y];
+        this->data[y] = x;
+        return true;
+    }
+
+    template <class F>
+    bool unite(int x, int y, F f) {
+        x = this->root(x), y = this->root(y);
+        if (x != y) {
+            if (this->data[x] > this->data[y]) swap(x, y);
+            this->data[x] += this->data[y];
+            this->data[y] = x;
+        }
+        f(x, y);
+        return x != y;
+    }
+
+  private:
+    std::vector<int> data;
+};
+#line 4 "a.cpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    pair<ll, ll> s, g;
+    cin >> s >> g;
+    vector<pair<ll, ll>> p(n);
+    vector<ll> d(n);
+    rep(i, n) {
+        cin >> p[i] >> d[i];
+    }
+
+    auto dist = [](auto p, auto q) {
+        return (p.first - q.first) * (p.first - q.first) +
+               (p.second - q.second) * (p.second - q.second);
+    };
+
+    union_find uf(n);
+    rep(i, n) {
+        FOR(j, i + 1, n) {
+            ll x = dist(p[i], p[j]);
+            if ((d[i] - d[j]) * (d[i] - d[j]) <= x && x <= (d[i] + d[j]) * (d[i] + d[j]))
+                uf.unite(i, j);
+        }
+    }
+
+    bool flag = false;
+    rep(i, n) {
+        if (dist(s, p[i]) != d[i] * d[i])
+            continue;
+        rep(j, n) {
+            if (dist(g, p[j]) != d[j] * d[j])
+                continue;
+            flag |= uf.same(i, j);
+        }
+    }
+    Yes(flag);
+
+    return 0;
+}
---
+++ code/abc/abc259/abc259_e.cpp
@@ -0,0 +1,189 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 2 "/home/kuhaku/home/github/algo/lib/algorithm/compress.hpp"
+
+/**
+ * @brief 座標圧縮
+ *
+ * @tparam T 要素の型
+ */
+template <class T>
+struct Compress {
+    Compress() {}
+    Compress(const vector<T>& _data) : data(_data) { this->build(); }
+
+    const T& operator[](int i) const { return this->data[i]; }
+    T& operator[](int i) { return this->data[i]; }
+
+    void add(T x) { this->data.emplace_back(x); }
+
+    void build() {
+        std::sort(this->data.begin(), this->data.end());
+        this->data.erase(std::unique(this->data.begin(), this->data.end()), this->data.end());
+    }
+    void build(const vector<T>& _data) {
+        this->data = _data;
+        std::sort(this->data.begin(), this->data.end());
+        this->data.erase(std::unique(this->data.begin(), this->data.end()), this->data.end());
+    }
+
+    bool exist(T x) const {
+        auto it = std::lower_bound(this->data.begin(), this->data.end(), x);
+        return it != this->data.end() && *it == x;
+    }
+
+    int get(T x) const {
+        auto it = std::lower_bound(this->data.begin(), this->data.end(), x);
+        return it - this->data.begin();
+    }
+
+    int size() const { return this->data.size(); }
+
+  private:
+    std::vector<T> data;
+};
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using ll = int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 4 "a.cpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> m(n);
+    vector<vector<int>> p(n), e(n);
+    Compress<int> cmp;
+    rep(i, n) {
+        cin >> m[i];
+        p[i].resize(m[i]);
+        e[i].resize(m[i]);
+        rep(j, m[i]) {
+            cin >> p[i][j] >> e[i][j];
+            cmp.add(p[i][j]);
+        }
+    }
+
+    cmp.build();
+    mt19937_64 mt;
+    vector<ll> r(cmp.size());
+    rep(i, r.size()) r[i] = mt();
+    vector<vector<int>> v(r.size());
+    rep(i, n) {
+        rep(j, m[i]) v[cmp.get(p[i][j])].emplace_back(e[i][j]);
+    }
+    for (auto &i : v) {
+        sort(all(i));
+        reverse(all(i));
+    }
+
+    ll x = 0;
+    for (auto i : r) x ^= i;
+    unordered_set<ll> st;
+    rep(i, n) {
+        ll y = x;
+        rep(j, m[i]) {
+            ll t = cmp.get(p[i][j]);
+            if (v[t].size() == 1 || v[t][0] == e[i][j] && v[t][0] != v[t][1]) {
+                y ^= r[t];
+            }
+        }
+        st.emplace(y);
+    }
+    co(st.size());
+
+    return 0;
+}

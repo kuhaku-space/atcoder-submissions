d7290aff32da6595b9c05330e92e8a7649c07f81
GitHub <noreply@github.com>
2022-09-05 10:23:06 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc262/abc262_d.cpp
@@ -0,0 +1,221 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/math/modint.hpp"
+
+/**
+ * @brief modint
+ * @see https://github.com/ei1333/library/blob/master/math/combinatorics/mod-int.cpp
+ *
+ * @tparam mod 法
+ */
+template <int mod = MOD_N>
+struct ModInt {
+    static constexpr int get_mod() noexcept { return mod; }
+
+    constexpr ModInt() noexcept : x(0) {}
+    constexpr ModInt(int y) noexcept : x(y >= 0 ? y % mod : (mod - 1 - ~y % mod)) {}
+    constexpr ModInt(std::int64_t y) noexcept : x(y >= 0 ? y % mod : (mod - 1 - ~y % mod)) {}
+
+    constexpr ModInt &operator+=(const ModInt &rhs) noexcept {
+        if ((this->x += rhs.x) >= mod) this->x -= mod;
+        return *this;
+    }
+    constexpr ModInt &operator-=(const ModInt &rhs) noexcept {
+        if ((this->x += mod - rhs.x) >= mod) this->x -= mod;
+        return *this;
+    }
+    constexpr ModInt &operator*=(const ModInt &rhs) noexcept {
+        this->x = (int)(1LL * this->x * rhs.x % mod);
+        return *this;
+    }
+    constexpr ModInt &operator/=(const ModInt &rhs) noexcept {
+        *this *= rhs.inverse();
+        return *this;
+    }
+
+    constexpr ModInt &operator++() noexcept {
+        if ((++(this->x)) >= mod) this->x -= mod;
+        return *this;
+    }
+    constexpr ModInt operator++(int) noexcept {
+        ModInt tmp(*this);
+        this->operator++();
+        return tmp;
+    }
+    constexpr ModInt &operator--() noexcept {
+        if ((this->x += mod - 1) >= mod) this->x -= mod;
+        return *this;
+    }
+    constexpr ModInt operator--(int) noexcept {
+        ModInt tmp(*this);
+        this->operator--();
+        return tmp;
+    }
+
+    constexpr ModInt operator-() const noexcept { return ModInt(-this->x); }
+    constexpr ModInt operator+(const ModInt &rhs) const noexcept { return ModInt(*this) += rhs; }
+    constexpr ModInt operator-(const ModInt &rhs) const noexcept { return ModInt(*this) -= rhs; }
+    constexpr ModInt operator*(const ModInt &rhs) const noexcept { return ModInt(*this) *= rhs; }
+    constexpr ModInt operator/(const ModInt &rhs) const noexcept { return ModInt(*this) /= rhs; }
+
+    constexpr bool operator==(const ModInt &rhs) const noexcept { return this->x == rhs.x; }
+    constexpr bool operator!=(const ModInt &rhs) const noexcept { return this->x != rhs.x; }
+
+    explicit operator int() const noexcept { return x; }
+
+    constexpr ModInt inverse() const noexcept {
+        int a = x, b = mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return ModInt(u);
+    }
+
+    constexpr ModInt pow(std::int64_t n) const noexcept { return ModInt(*this).pow_self(n); }
+    constexpr ModInt &pow_self(std::int64_t n) noexcept {
+        ModInt res(1);
+        for (; n > 0; n >>= 1) {
+            if (n & 1) res *= *this;
+            *this *= *this;
+        }
+        *this = res;
+        return *this;
+    }
+
+    friend std::istream &operator>>(std::istream &is, ModInt &rhs) {
+        std::int64_t t;
+        is >> t;
+        rhs = ModInt<mod>(t);
+        return (is);
+    }
+    friend std::ostream &operator<<(std::ostream &os, const ModInt &rhs) { return os << rhs.x; }
+
+  private:
+    int x;
+};
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using ll = int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 4 "a.cpp"
+
+using Mint = ModInt<>;
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+    Mint ans = 0;
+    repn(k, n) {
+        vector<vector<Mint>> v(k + 1, vector<Mint>(k));
+        v[0][0] = 1;
+        rep(i, n) {
+            int r = a[i] % k;
+            repr(j, k) {
+                rep(l, k) {
+                    v[j + 1][(l + r) % k] += v[j][l];
+                }
+            }
+        }
+        ans += v[k][0];
+    }
+    co(ans);
+
+    return 0;
+}

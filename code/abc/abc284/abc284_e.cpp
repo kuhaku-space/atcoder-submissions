f680f882b8fa3566e699bf145d89fa208d17acfe
GitHub <noreply@github.com>
2023-01-07 23:19:47 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc284/abc284_e.cpp
@@ -0,0 +1,257 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/graph/graph.hpp"
+
+/**
+ * @brief 重み付きグラフ
+ *
+ * @tparam T 辺の重みの型
+ */
+template <class T>
+struct Graph {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to(), _weight() {}
+        constexpr _edge(int from, int to, T weight) : _from(from), _to(to), _weight(weight) {}
+        constexpr bool operator<(const _edge &rhs) const { return this->weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+        constexpr int from() const { return this->_from; }
+        constexpr int to() const { return this->_to; }
+        constexpr T weight() const { return this->_weight; }
+
+      private:
+        int _from, _to;
+        T _weight;
+    };
+
+  public:
+    using edge_type = typename Graph<T>::_edge;
+
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return this->edges[i]; }
+    auto &operator[](int i) { return this->edges[i]; }
+    const auto begin() const { return this->edges.begin(); }
+    auto begin() { return this->edges.begin(); }
+    const auto end() const { return this->edges.end(); }
+    auto end() { return this->edges.end(); }
+    constexpr int size() const { return this->_size; }
+
+    void add_edge(const edge_type &e) { this->edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to, T weight = T(1)) {
+        this->edges[from].emplace_back(from, to, weight);
+    }
+    void add_edges(int from, int to, T weight = T(1)) {
+        this->edges[from].emplace_back(from, to, weight);
+        this->edges[to].emplace_back(to, from, weight);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            cin >> from >> to >> weight;
+            this->add_edge(from - base, to - base, weight);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            cin >> from >> to >> weight;
+            this->add_edges(from - base, to - base, weight);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+
+template <>
+struct Graph<void> {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to() {}
+        constexpr _edge(int from, int to) : _from(from), _to(to) {}
+
+        constexpr int from() const { return this->_from; }
+        constexpr int to() const { return this->_to; }
+        constexpr int weight() const { return 1; }
+        constexpr bool operator<(const _edge &rhs) const { return this->weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+      private:
+        int _from, _to;
+    };
+
+  public:
+    using edge_type = typename Graph<void>::_edge;
+
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return this->edges[i]; }
+    auto &operator[](int i) { return this->edges[i]; }
+    const auto begin() const { return this->edges.begin(); }
+    auto begin() { return this->edges.begin(); }
+    const auto end() const { return this->edges.end(); }
+    auto end() { return this->edges.end(); }
+    constexpr int size() const { return this->_size; }
+
+    void add_edge(const edge_type &e) { this->edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to) { this->edges[from].emplace_back(from, to); }
+    void add_edges(int from, int to) {
+        this->edges[from].emplace_back(from, to);
+        this->edges[to].emplace_back(to, from);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            cin >> from >> to;
+            this->add_edge(from - base, to - base);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            cin >> from >> to;
+            this->add_edges(from - base, to - base);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using ll = int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 4 "a.cpp"
+
+int main(void) {
+    int n, m;
+    cin >> n >> m;
+    Graph<void> g(n);
+    g.input_edges(m);
+
+    int ans = 0;
+    unordered_set<int> used;
+    auto dfs = [&](auto self, int x, int par) {
+        if (ans >= 1000000)
+            return;
+        ++ans;
+        for (auto e : g[x]) {
+            if (par == e.to())
+                continue;
+            if (used.count(e.to()))
+                continue;
+            used.emplace(e.to());
+            self(self, e.to(), par);
+            used.erase(e.to());
+            if (ans >= 1000000)
+                return;
+        }
+    };
+
+    used.emplace(0);
+    dfs(dfs, 0, -1);
+    co(ans);
+
+    return 0;
+}

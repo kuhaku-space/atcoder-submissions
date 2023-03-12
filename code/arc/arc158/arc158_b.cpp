b15765fab1d5069dd2dfc0e04fd8edbdb5e91982
GitHub <noreply@github.com>
2023-03-12 23:19:36 +0000

    [auto-commit] Add my codes

---
+++ code/arc/arc158/arc158_b.cpp
@@ -0,0 +1,156 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/atcoder/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/atcoder/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/atcoder/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/atcoder/github/algo/lib/template/atcoder.hpp"
+using ll = int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 3 "a.cpp"
+
+ll f(const multiset<ll> &st1, const multiset<ll> &st2, int q) {
+    if (q == 0) {
+        return *st1.begin();
+    } else if (q == 1) {
+        return -(*st2.begin());
+    } else if (q == 2) {
+        auto it = st1.lower_bound(0);
+        if (it == st1.end())
+            return *st1.begin();
+        return *it;
+    } else {
+        auto it = st2.lower_bound(0);
+        if (it == st2.end())
+            return -(*st2.begin());
+        return -(*it);
+    }
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<ll> x(n);
+    cin >> x;
+    vector<ll> y = x;
+    for_each(all(y), [](auto &a) {
+        a = -a;
+    });
+    multiset<ll> st1(all(x)), st2(all(y));
+
+    ld mi = Inf, ma = -Inf;
+    rep (i, 4) {
+        auto a = f(st1, st2, i);
+        st1.erase(st1.find(a));
+        st2.erase(st2.find(-a));
+        rep (j, 4) {
+            auto b = f(st1, st2, j);
+            st1.erase(st1.find(b));
+            st2.erase(st2.find(-b));
+            rep (k, 4) {
+                auto c = f(st1, st2, k);
+                chmin(mi, (ld)(a + b + c) / a / b / c);
+                chmax(ma, (ld)(a + b + c) / a / b / c);
+            }
+            st1.emplace(b);
+            st2.emplace(-b);
+        }
+        st1.emplace(a);
+        st2.emplace(-a);
+    }
+    setp(15);
+    co(mi);
+    co(ma);
+
+    return 0;
+}

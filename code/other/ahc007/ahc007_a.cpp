f2a84f30475948e83d2d7f6748fb2f54eb03d08b
GitHub <noreply@github.com>
2023-03-14 23:20:46 +0000

    [auto-commit] Add my codes

--- code/other/ahc007/ahc007_a.cpp
+++ code/other/ahc007/ahc007_a.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  * @author kuhaku-space (kuhakuspace2000@gmail.com)
  * @brief AHC007 solution
- * @version 0.1
+ * @version 1.0
  * @date 2021-12-12
  * @details [問題](https://atcoder.jp/contests/ahc007/tasks/ahc007_a)
  *
@@ -29,7 +29,7 @@
 
 using namespace std;
 #define FOR(i, m, n) for (int i = (m); i < (n); ++i)
-#define rep(i, n) FOR(i, 0, n)
+#define rep(i, n) FOR (i, 0, n)
 
 constexpr int N = 400;
 constexpr int M = 1995;
@@ -79,13 +79,13 @@ struct union_find {
 
     bool is_root(int x) { return this->data[x] < 0; }
 
-    void unite(int x, int y) {
+    bool unite(int x, int y) {
         x = this->root(x), y = this->root(y);
-        if (x != y) {
-            if (this->data[x] > this->data[y]) swap(x, y);
-            this->data[x] += this->data[y];
-            this->data[y] = x;
-        }
+        if (x == y) return false;
+        if (this->data[x] > this->data[y]) swap(x, y);
+        this->data[x] += this->data[y];
+        this->data[y] = x;
+        return true;
     }
 
     int size(int x) { return -(this->data[this->root(x)]); }
@@ -96,33 +96,40 @@ struct union_find {
 };
 // clang-format on
 
-Xorshift xor128;
+Xorshift rnd;
 
-constexpr int TRY_TIME = 150;
+constexpr int TRY_TIME = 100;
+array<array<int, M>, TRY_TIME> drand;
+
+void set_drand() {
+    rep (i, TRY_TIME) {
+        rep (j, M) {
+            drand[i][j] = dmin[j] + rnd() % (dmax[j] - dmin[j] + 1);
+        }
+    }
+}
 
 int main(void) {
-    rep(i, N) cin >> x[i] >> y[i];
-    rep(i, M) cin >> u[i] >> v[i];
+    rep (i, N) cin >> x[i] >> y[i];
+    rep (i, M) cin >> u[i] >> v[i];
 
-    rep(i, M) {
+    rep (i, M) {
         d[i] = dist(u[i], v[i]);
         dmax[i] = d[i] * 2.87, dmin[i] = d[i] * 1.13;
     }
+    set_drand();
 
-    array<int, M> cnt;
-    cnt.fill(0);
-    rep(i, TRY_TIME) {
+    array<int, M> cnt = {};
+    rep (loop, TRY_TIME) {
         vector<pair<int, int>> ps;
-        rep(i, M) {
-            ps.emplace_back(dmin[i] + xor128() % (dmax[i] - dmin[i] + 1), i);
+        rep (i, M) {
+            ps.emplace_back(drand[loop][i], i);
         }
         sort(ps.begin(), ps.end());
         union_find uf;
         for (auto p : ps) {
-            if (!uf.same(u[p.second], v[p.second])) {
+            if (uf.unite(u[p.second], v[p.second]))
                 ++cnt[p.second];
-                uf.unite(u[p.second], v[p.second]);
-            }
         }
     }
 
@@ -130,10 +137,10 @@ int main(void) {
     union_find uf;
     vector<int> idx;
     for (int i = M - 1; i >= 0; --i) {
-        if (cnt[i] > 5)
+        if (cnt[i] >= 1)
             idx.emplace_back(i);
     }
-    rep(m, M) {
+    rep (m, M) {
         cin >> l[m];
         if (idx.empty() || idx.back() != m) {
             cout << 0 << endl;
@@ -141,36 +148,33 @@ int main(void) {
         }
         idx.pop_back();
         int eval = 0;
-        rep(_, 45) {
-            if (cnt[m] >= TRY_TIME - 5) {
+        rep (loop, 25) {
+            if (cnt[m] == TRY_TIME)
                 break;
-            }
+
             vector<pair<int, int>> ps;
             for (auto i : idx) {
-                ps.emplace_back(dmin[i] + xor128() % (dmax[i] - dmin[i] + 1), i);
+                ps.emplace_back(drand[loop][i], i);
             }
             sort(ps.begin(), ps.end());
-            int scorea = score + l[m], scoreb = score;
+            int score_a = score + l[m], score_b = score;
             union_find ufa = uf, ufb = uf;
             ufa.unite(u[m], v[m]);
             for (auto p : ps) {
-                if (!ufa.same(u[p.second], v[p.second])) {
-                    ufa.unite(u[p.second], v[p.second]);
-                    scorea += p.first;
-                }
-                if (!ufb.same(u[p.second], v[p.second])) {
-                    ufb.unite(u[p.second], v[p.second]);
-                    scoreb += p.first;
-                }
-                if (_ && ufa.size(u[m]) == ufb.size(u[m]))
+                if (ufa.unite(u[p.second], v[p.second]))
+                    score_a += p.first;
+                if (ufb.unite(u[p.second], v[p.second]))
+                    score_b += p.first;
+
+                if (loop && ufa.size(u[m]) == ufb.size(u[m]))
                     break;
             }
 
-            if (_ == 0 && ufb.size(0) != N) {
+            if (loop == 0 && ufb.size(0) != N) {
                 eval = 1;
                 break;
             }
-            eval += scoreb - scorea;
+            eval += score_b - score_a;
         }
         if (eval >= 0) {
             cout << 1 << endl;

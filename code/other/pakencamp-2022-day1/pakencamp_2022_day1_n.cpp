3ebaa36f2a680a3b92aa2ad42720dd4e6ebc85d3
GitHub <noreply@github.com>
2023-04-04 23:18:34 +0000

    [auto-commit] Add my codes

---
+++ code/other/pakencamp-2022-day1/pakencamp_2022_day1_n.cpp
@@ -0,0 +1,170 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using ll = int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 3 "a.cpp"
+
+void solve() {
+    ll s, t, p;
+    cin >> s >> t >> p;
+    vector<ll> a(3), b(3);
+    rep (i, 3) cin >> a[i] >> b[i];
+
+    if (s == t) {
+        co(0);
+        return;
+    }
+
+    vector<unordered_set<ll>> st(3);
+    ll m = sqrt(3000000000) + 1;
+    rep (i, 3) {
+        ll x = t;
+        int cnt = i;
+        rep (_, m) {
+            x = (a[cnt] * x + b[cnt]) % p;
+            (++cnt) %= 3;
+            st[cnt].emplace(x);
+        }
+    }
+
+    ll x = s;
+    int cnt = 0;
+    vector<ll> c(3, 1), d(3, 0);
+    rep (i, m) {
+        x = (a[cnt] * x + b[cnt]) % p;
+        if (x == t) {
+            co(i + 1);
+            return;
+        }
+        rep (i, 3) {
+            d[i] = (a[cnt] * d[i] + b[cnt]) % p;
+            c[i] = a[cnt] * c[i] % p;
+            (++cnt) %= 3;
+        }
+        (++cnt) %= 3;
+    }
+
+    x = s;
+    cnt = 0;
+    rep (i, m) {
+        ll y = (c[cnt] * x + d[cnt]) % p;
+        ll e = (cnt + m) % 3;
+        if (st[e].count(y)) {
+            rep (j, m) {
+                x = (a[cnt] * x + b[cnt]) % p;
+                if (x == t) {
+                    co(i * m + j + 1);
+                    return;
+                }
+                (++cnt) %= 3;
+            }
+            co(-1);
+            return;
+        }
+        x = y, cnt = e;
+    }
+    co(-1);
+}
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}

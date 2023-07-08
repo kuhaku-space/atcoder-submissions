a18bff4c278809f074475ca481c84bdcfe84d1c4
GitHub <noreply@github.com>
2023-07-08 23:20:14 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc309/abc309_f.cpp
@@ -0,0 +1,475 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr int MOD = 1000000007;
+constexpr int MOD_N = 998244353;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/algorithm/compress.hpp"
+
+/**
+ * @brief 座標圧縮
+ *
+ * @tparam T 要素の型
+ */
+template <class T>
+struct coordinate_compression {
+    coordinate_compression() = default;
+    coordinate_compression(const std::vector<T> &_data) : data(_data) { this->build(); }
+
+    const T &operator[](int i) const { return this->data[i]; }
+    T &operator[](int i) { return this->data[i]; }
+
+    void add(T x) { this->data.emplace_back(x); }
+
+    void build() {
+        std::sort(this->data.begin(), this->data.end());
+        this->data.erase(std::unique(this->data.begin(), this->data.end()), this->data.end());
+    }
+    void build(const std::vector<T> &v) {
+        this->data = v;
+        std::sort(this->data.begin(), this->data.end());
+        this->data.erase(std::unique(this->data.begin(), this->data.end()), this->data.end());
+    }
+
+    bool exist(T x) const {
+        auto it = std::lower_bound(this->data.begin(), this->data.end(), x);
+        return it != this->data.end() && *it == x;
+    }
+
+    int get(T x) const {
+        auto it = std::lower_bound(this->data.begin(), this->data.end(), x);
+        return it - this->data.begin();
+    }
+
+    int size() const { return this->data.size(); }
+
+  private:
+    std::vector<T> data;
+};
+
+/**
+ * @brief 座標圧縮
+ *
+ * @tparam T 要素の型
+ * @param v
+ * @return std::vector<T>
+ */
+template <class T>
+std::vector<T> compress(const std::vector<T> &v) {
+    coordinate_compression cps(v);
+    std::vector<T> res;
+    for (auto x : v) res.emplace_back(cps.get(x));
+    return res;
+}
+#line 3 "/home/kuhaku/home/github/algo/lib/math/pow.hpp"
+
+std::int64_t safe_mod(std::int64_t a, std::int64_t mod) {
+    a %= mod;
+    return a >= 0 ? a : a + mod;
+}
+
+std::int64_t pow_int(std::int64_t a, std::int64_t n) {
+    assert(n >= 0);
+    std::int64_t res = 1, mul = a;
+    for (; n > 0; n >>= 1) {
+        if (n & 1) res *= mul;
+        mul *= mul;
+    }
+    return res;
+}
+
+std::int64_t inv_mod(std::int64_t a, std::int64_t mod) {
+    std::int64_t b = mod, u = 1, v = 0, t;
+    while (b > 0) {
+        t = a / b;
+        std::swap(a -= t * b, b);
+        std::swap(u -= t * v, v);
+    }
+    return u >= 0 ? u % mod : (mod - (-u) % mod) % mod;
+}
+
+std::int64_t pow_mod(std::int64_t a, std::int64_t n, std::int64_t mod) {
+    if (n < 0) return inv_mod(pow_mod(a, -n, mod), mod);
+    std::int64_t res = 1, mul = safe_mod(a, mod);
+    for (; n > 0; n >>= 1) {
+        if (n & 1) (res *= mul) %= mod;
+        (mul *= mul) %= mod;
+    }
+    return res;
+}
+
+int ceil_pow2(std::int64_t n) {
+    int x = 0;
+    while ((std::uint64_t(1) << x) < (std::uint64_t)(n)) ++x;
+    return x;
+}
+
+int floor_pow2(std::int64_t n) {
+    int x = 0;
+    while ((std::uint64_t(1) << (x + 1)) <= (std::uint64_t)(n)) ++x;
+    return x;
+}
+#line 3 "/home/kuhaku/home/github/algo/lib/segment_tree/monoid.hpp"
+
+template <class T>
+struct Add {
+    using value_type = T;
+    static constexpr T id = T(0);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs + rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs + rhs;
+    }
+};
+
+template <class T>
+struct And {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs & rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs & rhs;
+    }
+};
+
+template <class T>
+struct Or {
+    using value_type = T;
+    static constexpr T id = T(0);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs | rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs | rhs;
+    }
+};
+
+template <class T>
+struct Xor {
+    using value_type = T;
+    static constexpr T id = T(0);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs ^ rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs ^ rhs;
+    }
+};
+
+template <class T>
+struct Min {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) { return std::min(lhs, rhs); }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return std::min((U)lhs, rhs);
+    }
+};
+
+template <class T>
+struct Max {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::min();
+    static constexpr T op(const T &lhs, const T &rhs) { return std::max(lhs, rhs); }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return std::max((U)lhs, rhs);
+    }
+};
+
+template <class T>
+struct Update {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs == Update::id ? rhs : lhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs == Update::id ? rhs : lhs;
+    }
+};
+
+template <class T>
+struct Affine {
+    using value_type = std::pair<T, T>;
+    static constexpr std::pair<T, T> id = std::pair<T, T>(1, 0);
+    static constexpr std::pair<T, T> op(std::pair<T, T> lhs, std::pair<T, T> rhs) {
+        return {lhs.first * rhs.first, lhs.first * rhs.second + lhs.second};
+    }
+};
+
+template <class M>
+struct Rev {
+    using T = typename M::value_type;
+    using value_type = T;
+    static constexpr T id = M::id;
+    static constexpr T op(T lhs, T rhs) { return M::op(rhs, lhs); }
+};
+#line 5 "/home/kuhaku/home/github/algo/lib/segment_tree/segment_tree.hpp"
+
+/**
+ * @brief セグメント木
+ * @see https://noshi91.hatenablog.com/entry/2020/04/22/212649
+ *
+ * @tparam M モノイド
+ */
+template <class M>
+struct segment_tree {
+  private:
+    using T = typename M::value_type;
+
+  public:
+    segment_tree() : segment_tree(0) {}
+    explicit segment_tree(int n, T e = M::id) : segment_tree(std::vector<T>(n, e)) {}
+    template <class U>
+    explicit segment_tree(const std::vector<U> &v) : _n(v.size()) {
+        this->_log = ceil_pow2(this->_n);
+        this->_size = 1 << this->_log;
+        this->data = std::vector<T>(this->_size << 1, M::id);
+        for (int i = 0; i < this->_n; ++i) this->data[this->_size + i] = T(v[i]);
+        for (int i = this->_size - 1; i >= 1; --i) this->update(i);
+    }
+
+    const T &operator[](int k) const { return this->data[k + this->_size]; }
+    T at(int k) const { return this->operator[](k); }
+    T get(int k) const { return this->operator[](k); }
+
+    void set(int k, T val) {
+        assert(0 <= k && k < this->_n);
+        k += this->_size;
+        this->data[k] = val;
+        for (int i = 1; i <= this->_log; ++i) this->update(k >> i);
+    }
+    void reset(int k) { this->set(k, M::id); }
+
+    T all_prod() const { return this->data[1]; }
+    T prod(int a, int b) const {
+        assert(0 <= a && b <= this->_n);
+        T l = M::id, r = M::id;
+        for (a += this->_size, b += this->_size; a < b; a >>= 1, b >>= 1) {
+            if (a & 1) l = M::op(l, this->data[a++]);
+            if (b & 1) r = M::op(this->data[--b], r);
+        }
+        return M::op(l, r);
+    }
+
+    template <class F>
+    int max_right(F f) const {
+        return this->max_right(0, f);
+    }
+
+    template <class F>
+    int max_right(int l, F f) const {
+        assert(0 <= l && l <= this->_n);
+        assert(f(M::id));
+        if (l == this->_n) return this->_n;
+        l += this->_size;
+        T sm = M::id;
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(M::op(sm, this->data[l]))) {
+                while (l < this->_size) {
+                    l = (2 * l);
+                    if (f(M::op(sm, this->data[l]))) {
+                        sm = M::op(sm, this->data[l]);
+                        l++;
+                    }
+                }
+                return l - this->_size;
+            }
+            sm = M::op(sm, this->data[l]);
+            l++;
+        } while ((l & -l) != l);
+        return this->_n;
+    }
+
+    int max_right(T x) const { return this->max_right(0, x); }
+
+    int max_right(int l, T x) const {
+        return this->max_right(l, [&x](auto y) {
+            return !(y < x);
+        });
+    }
+
+    template <class F>
+    int min_left(F f) const {
+        return this->min_left(this->_n, f);
+    }
+
+    template <class F>
+    int min_left(int r, F f) const {
+        assert(0 <= r && r <= this->_n);
+        assert(f(M::id));
+        if (r == 0) return 0;
+        r += this->_size;
+        T sm = M::id;
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(M::op(this->data[r], sm))) {
+                while (r < this->_size) {
+                    r = (2 * r + 1);
+                    if (f(M::op(this->data[r], sm))) {
+                        sm = M::op(this->data[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - this->_size;
+            }
+            sm = M::op(this->data[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+    int min_left(T x) const { return this->min_left(this->_n, x); }
+
+    int min_left(int r, T x) const {
+        return this->min_left(r, [&x](auto y) {
+            return !(y < x);
+        });
+    }
+
+  private:
+    int _n, _size, _log;
+    std::vector<T> data;
+
+    void update(int k) { this->data[k] = M::op(this->data[2 * k], this->data[2 * k + 1]); }
+};
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void setp(int n) {
+    std::cout << std::fixed << std::setprecision(n);
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 5 "a.cpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<tuple<int, int, int>> a(n);
+    coordinate_compression<int> cx, cy, cz;
+    rep (i, n) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        vector<int> t = {x, y, z};
+        sort(all(t));
+        x = t[0], y = t[1], z = t[2];
+        a[i] = {x, y, z};
+        cx.add(x), cy.add(y), cz.add(z);
+    }
+    cx.build(), cy.build(), cz.build();
+
+    vector<vector<pair<int, int>>> v(n);
+    for (auto &[x, y, z] : a) {
+        x = cx.get(x), y = cy.get(y), z = cz.get(z);
+        v[x].emplace_back(n - 1 - y, n - 1 - z);
+    }
+
+    segment_tree<Min<int>> st(n);
+    reverse(all(v));
+    for (auto &u : v) {
+        for (auto [x, y] : u) {
+            if (st.prod(0, x) < y) {
+                Yes();
+                return 0;
+            }
+        }
+        for (auto [x, y] : u) st.set(x, min(st.get(x), y));
+    }
+    No();
+
+    return 0;
+}

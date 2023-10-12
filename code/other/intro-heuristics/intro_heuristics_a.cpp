e92b1e5e96acda9a6865067f594593d571b24fca
GitHub <noreply@github.com>
2023-10-12 23:18:42 +0000

    [auto-commit] Add my codes

--- code/other/intro-heuristics/intro_heuristics_a.cpp
+++ code/other/intro-heuristics/intro_heuristics_a.cpp
@@ -1,15 +1,3 @@
-/**
- * @file main.cpp
- * @author kuhaku-space (kuhakuspace2000@gmail.com)
- * @brief Introduction to Heuristics Contest solution
- * @version 0.1
- * @date 2021-12-13
- * @details [問題](https://atcoder.jp/contests/intro-heuristics/tasks/intro_heuristics_a)
- *
- * @copyright Copyright (c) 2021
- *
- */
-
 #ifndef LOCAL
 #pragma GCC optimize("O3")
 #pragma GCC optimize("unroll-loops")
@@ -21,75 +9,64 @@
 #include <chrono>
 #include <cmath>
 #include <iostream>
+#include <numeric>
 #include <queue>
 #include <set>
 #include <tuple>
 #include <vector>
 
-using namespace std;
-using ll = int64_t;
-#define FOR(i, m, n) for (int i = (m); i < (n); ++i)
-#define rep(i, n) FOR (i, 0, n)
 template <class T, class U>
-bool chmax(T &a, const U &b) {
-    return a < b ? a = b, true : false;
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
 }
 template <class T, class U>
-bool chmin(T &a, const U &b) {
-    return b < a ? a = b, true : false;
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
 }
-constexpr int INF = 1000000003;
-constexpr ll LINF = 1000000000000000003;
 
-constexpr double T0 = 2e3, T1 = 6e2;
+namespace lib {
 
-// clang-format off
 struct Xorshift {
     using result_type = unsigned int;
     constexpr Xorshift() : x(123456789), y(362436069), z(521288629), w(88675123) {}
-    constexpr Xorshift(result_type _w) : x(123456789), y(362436069), z(521288629), w(_w) {}
 
-    constexpr result_type min() const { return std::numeric_limits<result_type>::min(); }
-    constexpr result_type max() const { return std::numeric_limits<result_type>::max(); }
+    static constexpr result_type min() { return std::numeric_limits<result_type>::min(); }
+    static constexpr result_type max() { return std::numeric_limits<result_type>::max(); }
     result_type operator()() {
         result_type t = (x ^ (x << 11));
         x = y, y = z, z = w;
         return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
     }
 
-    result_type rand_range(int a, int b) { return a + this->operator()() % (b - a + 1); }
-    double rand_d() { return (double)this->operator()() / this->max(); }
+    result_type rand_range(int a, int b) { return a + operator()() % (b - a + 1); }
+    double rand_d() { return (double)operator()() / max(); }
 
   private:
     result_type x, y, z, w;
 };
 
+using namespace std::chrono;
+
 struct Timer {
-    std::chrono::system_clock::time_point start;
+    Timer() : start(system_clock::now()) {}
 
-    Timer() : start(std::chrono::system_clock::now()) {}
+    std::int64_t operator()() const { return get_time(); }
 
-    int64_t operator()() const { return this->get_time(); }
-
-    int64_t get_time() const {
-        return std::chrono::duration_cast<std::chrono::milliseconds>(
-                   std::chrono::system_clock::now() - start)
-            .count();
+    std::int64_t get_time() const {
+        return duration_cast<milliseconds>(system_clock::now() - start).count();
     }
 
-    void reset() { this->start = std::chrono::system_clock::now(); }
+    void reset() { start = system_clock::now(); }
+
+  private:
+    system_clock::time_point start;
 };
 
 template <int TL>
 struct SA {
-    Timer timer;
-    Xorshift xor128;
-
     SA() : timer(), xor128() {}
 
-    auto get_time() {
-        return this->timer.get_time();
-    }
+    auto get_time() { return timer.get_time(); }
 
     /**
      * @brief 温度関数
@@ -99,11 +76,9 @@ struct SA {
      */
     double temperature(double t) {
         t /= TL;
-        return pow(T0, 1.0 - t) * pow(T1, t);
+        return std::pow(T0, 1.0 - t) * std::pow(T1, t);
     }
-    double temperature() {
-        return this->temperature(this->get_time());
-    }
+    double temperature() { return temperature(get_time()); }
 
     /**
      * @brief 遷移関数
@@ -116,23 +91,14 @@ struct SA {
      */
     template <class T>
     bool is_update(T diff_score, double temp) {
-        if (diff_score >= 0)
-            return true;
-        return exp((double)diff_score / temp) >= this->xor128.rand_d();
+        if (diff_score >= 0) return true;
+        return std::exp((double)diff_score / temp) >= xor128.rand_d();
     }
-};
-// clang-format on
 
-Xorshift xor128;
-
-constexpr int SZ = 26;
-constexpr int D = 365;
-array<int, SZ> C;
-int SUM = 0;
-array<array<int, SZ>, D> S;
-
-struct Operation {
-    int d, t;
+  private:
+    static constexpr double T0 = 2e3, T1 = 6e2;
+    Timer timer;
+    Xorshift xor128;
 };
 
 template <class T>
@@ -142,19 +108,30 @@ struct Trace {
         return log.size() - 1;
     }
 
-    std::pair<T, int> get(int idx) {
-        return log[idx];
-    }
+    std::pair<T, int> get(int idx) { return log[idx]; }
 
   private:
     std::vector<std::pair<T, int>> log;
 };
-Trace<Operation> trace;
+
+}  // namespace lib
+
+constexpr int CONTEST_SIZE = 26;
+constexpr int D = 365;
+std::array<int, CONTEST_SIZE> C;
+int SUM = 0;
+std::array<std::array<int, CONTEST_SIZE>, D> S;
+
+struct Operation {
+    int d, t;
+};
+
+lib::Trace<Operation> trace;
 
 template <int BEAM_SIZE>
 struct beam_search {
     struct State {
-        array<short, SZ> last;
+        std::array<short, CONTEST_SIZE> last;
         int score, sum, trace_idx;
 
         State() : last{}, score(), sum(), trace_idx(-1) {}
@@ -170,9 +147,7 @@ struct beam_search {
             return score + S[d][t] + C[t] * (d + 1 - last[t]) - sum - SUM;
         }
 
-        int eval() const {
-            return score;
-        }
+        int eval() const { return score; }
 
         State apply(Operation op) const {
             State res{*this};
@@ -185,8 +160,8 @@ struct beam_search {
             update_score(op);
         }
 
-        array<char, D> answer() {
-            array<char, D> ans = {};
+        std::array<char, D> answer() {
+            std::array<char, D> ans = {};
             int idx = trace_idx;
             while (idx != -1) {
                 auto [op, prev_idx] = trace.get(idx);
@@ -198,30 +173,28 @@ struct beam_search {
     };
 
     auto solve() {
-        array<State, BEAM_SIZE> cur, nxt;
+        std::array<State, BEAM_SIZE> cur, nxt;
         cur[0] = State();
         int cur_size = 1;
-        rep (d, D) {
-            vector<tuple<int, int, Operation>> eval_que;
-            eval_que.reserve(cur_size * SZ);
+        for (int d = 0; d < D; ++d) {
+            std::vector<std::tuple<int, int, Operation>> eval_que;
+            eval_que.reserve(cur_size * CONTEST_SIZE);
             for (int i = 0; i < cur_size; ++i) {
-                rep (t, SZ) {
+                for (int t = 0; t < CONTEST_SIZE; ++t) {
                     int eval = cur[i].next_eval(d, t);
                     eval_que.emplace_back(eval, i, Operation{d, t});
                 }
             }
             if (eval_que.size() > BEAM_SIZE) {
-                nth_element(eval_que.begin(), eval_que.begin() + BEAM_SIZE, eval_que.end(),
-                            [](auto x, auto y) {
-                                return get<0>(x) > get<0>(y);
-                            });
+                std::nth_element(eval_que.begin(), eval_que.begin() + BEAM_SIZE, eval_que.end(),
+                                 [](auto x, auto y) { return std::get<0>(x) > std::get<0>(y); });
             }
-            int nxt_size = 0;
-            for (int i = 0; i < min(BEAM_SIZE, (int)eval_que.size()); i++) {
+            int nxt_size = std::min(BEAM_SIZE, (int)eval_que.size());
+            for (int i = 0; i < nxt_size; ++i) {
                 auto &&[eval, prev_idx, operation] = eval_que[i];
                 nxt[i] = cur[prev_idx].apply(operation);
             }
-            swap(cur, nxt);
+            std::swap(cur, nxt);
             cur_size = nxt_size;
         }
 
@@ -237,36 +210,28 @@ template <int TL>
 struct annealing {
     struct State {
         int score;
-        array<char, D> ans;
-        array<vector<int>, SZ> memo;
+        std::array<char, D> ans;
+        std::array<std::vector<int>, CONTEST_SIZE> memo;
 
-        State(const array<char, D> &arr) : ans(arr) {}
+        State(const std::array<char, D> &arr) : ans(arr) {}
 
-        auto get_score() const {
-            return score;
-        }
+        auto get_score() const { return score; }
 
-        auto get_char(int d) const {
-            return ans[d];
-        }
+        auto get_char(int d) const { return ans[d]; }
 
         void build() {
             score = 0;
             int sum = 0;
-            array<short, SZ> last = {};
-            rep (i, SZ) {
-                memo[i].emplace_back(-1);
-            }
-            rep (d, D) {
+            std::array<short, CONTEST_SIZE> last = {};
+            for (int i = 0; i < CONTEST_SIZE; ++i) { memo[i].emplace_back(-1); }
+            for (int d = 0; d < D; ++d) {
                 memo[ans[d]].emplace_back(d);
                 char t = ans[d];
                 sum += SUM - C[t] * (d + 1 - last[t]);
                 score += S[d][t] - sum;
                 last[t] = d + 1;
             }
-            rep (i, SZ) {
-                memo[i].emplace_back(D);
-            }
+            for (int i = 0; i < CONTEST_SIZE; ++i) { memo[i].emplace_back(D); }
         }
 
         auto update(int d, char e) {
@@ -286,13 +251,12 @@ struct annealing {
         }
     };
 
-    SA<TL> sa;
+    lib::SA<TL> sa;
+    lib::Xorshift xor128;
 
-    int get_time() {
-        return sa.get_time();
-    }
+    int get_time() { return sa.get_time(); }
 
-    int solve(array<char, D> &ans) {
+    int solve(std::array<char, D> &ans) {
         State state(ans);
         state.build();
         int max_score = state.get_score();
@@ -300,10 +264,10 @@ struct annealing {
         int score = max_score;
         while (sa.get_time() < TL) {
             double temp = sa.temperature();
-            rep (i, 300) {
+            for (int loop = 0; loop < 300; ++loop) {
                 if (xor128() % 2) {
                     int d = xor128() % D;
-                    char e = xor128() % SZ;
+                    char e = xor128() % CONTEST_SIZE;
                     auto p = state.update(d, e);
                     int new_score = state.get_score();
                     if (!sa.is_update(new_score - score, temp)) {
@@ -313,7 +277,7 @@ struct annealing {
                     }
                 } else {
                     int d1 = xor128() % (D - 1);
-                    int d2 = min((int)(d1 + 1 + xor128() % 10), D - 1);
+                    int d2 = std::min((int)(d1 + 1 + xor128() % 10), D - 1);
                     auto q = state.get_char(d2);
                     auto p = state.update(d1, q);
                     q = state.update(d2, p);
@@ -326,9 +290,7 @@ struct annealing {
                     }
                 }
 
-                if (chmax(max_score, score)) {
-                    ans = state.ans;
-                }
+                if (chmax(max_score, score)) { ans = state.ans; }
             }
         }
 
@@ -336,20 +298,33 @@ struct annealing {
     }
 };
 
+int calc_score(const std::array<char, D> &ans) {
+    int score = 1000000, sum = 0;
+    std::array<short, CONTEST_SIZE> last{};
+    for (int d = 0; d < D; ++d) {
+        int t = ans[d];
+        sum += SUM - C[t] * (d + 1 - last[t]);
+        score += S[d][t] - sum;
+        last[t] = d + 1;
+    }
+    return score;
+}
+
 int main(void) {
     int tmp;
-    cin >> tmp;
-    rep (i, SZ) cin >> C[i];
-    rep (i, SZ) SUM += C[i];
-    rep (i, D)
-        rep (j, SZ) cin >> S[i][j];
+    std::cin >> tmp;
+    for (int i = 0; i < CONTEST_SIZE; ++i) { std::cin >> C[i]; }
+    SUM = std::reduce(std::begin(C), std::end(C));
+    for (int i = 0; i < D; ++i) {
+        for (int j = 0; j < CONTEST_SIZE; ++j) { std::cin >> S[i][j]; }
+    }
 
     beam_search<4000> solver1;
     annealing<1985> solver2;
     auto ans = solver1.solve();
-    cerr << (double)clock() / CLOCKS_PER_SEC << endl;
-    cerr << "Score = " << solver2.solve(ans) + 1000000 << endl;
-    for (auto i : ans) cout << (int)i + 1 << endl;
+    std::cerr << (double)clock() / CLOCKS_PER_SEC << ' ' << calc_score(ans) << std::endl;
+    std::cerr << "Score = " << solver2.solve(ans) + 1000000 << std::endl;
+    for (auto i : ans) std::cout << (int)i + 1 << std::endl;
 
     return 0;
 }

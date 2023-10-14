ac6ef3db09a29853ee2e01e16e148e1864cc957c
GitHub <noreply@github.com>
2023-10-14 23:17:05 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc324/abc324_e.cpp
@@ -0,0 +1,311 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/binary_tree/fenwick_tree.hpp"
+#include <cassert>
+#include <vector>
+
+/**
+ * @brief フェニック木
+ *
+ * @tparam T
+ *
+ * @see http://hos.ac/slides/20140319_bit.pdf
+ */
+template <class T>
+struct fenwick_tree {
+    fenwick_tree() : _size(), data() {}
+    fenwick_tree(int n) : _size(n + 1), data(n + 1) {}
+    fenwick_tree(const std::vector<T> &v) : _size((int)v.size() + 1), data((int)v.size() + 1) {
+        this->build(v);
+    }
+    template <class U>
+    fenwick_tree(const std::vector<U> &v) : _size((int)v.size() + 1), data((int)v.size() + 1) {
+        this->build(v);
+    }
+
+    T operator[](int i) const { return this->sum(i + 1) - this->sum(i); }
+    T at(int k) const { return this->operator[](k); }
+    T get(int k) const { return this->operator[](k); }
+
+    template <class U>
+    void build(const std::vector<U> &v) {
+        for (int i = 0, n = v.size(); i < n; ++i) this->add(i, v[i]);
+    }
+
+    /**
+     * @brief v[k] = val
+     *
+     * @param k index of array
+     * @param val new value
+     * @return void
+     */
+    void update(int k, T val) { this->add(k, val - this->at(k)); }
+    /**
+     * @brief v[k] += val
+     *
+     * @param k index of array
+     * @param val new value
+     * @return void
+     */
+    void add(int k, T val) {
+        assert(0 <= k && k < this->_size);
+        for (++k; k < this->_size; k += k & -k) this->data[k] += val;
+    }
+    /**
+     * @brief chmax(v[k], val)
+     *
+     * @param k index of array
+     * @param val new value
+     * @return bool
+     */
+    bool chmax(int k, T val) {
+        if (this->at(k) >= val) return false;
+        this->update(k, val);
+        return true;
+    }
+    /**
+     * @brief chmin(v[k], val)
+     *
+     * @param k index of value
+     * @param val new value
+     * @return bool
+     */
+    bool chmin(int k, T val) {
+        if (this->at(k) <= val) return false;
+        this->update(k, val);
+        return true;
+    }
+
+    /**
+     * @brief v[0] + ... + v[n - 1]
+     *
+     * @return T
+     */
+    T all_sum() const { return this->sum(this->_size); }
+    /**
+     * @brief v[0] + ... + v[k - 1]
+     *
+     * @param k index of array
+     * @return T
+     */
+    T sum(int k) const {
+        assert(0 <= k && k <= this->_size);
+        T res = 0;
+        for (; k > 0; k -= k & -k) res += this->data[k];
+        return res;
+    }
+    /**
+     * @brief v[a] + ... + v[b - 1]
+     *
+     * @param a first index of array
+     * @param b last index of array
+     * @return T
+     */
+    T sum(int a, int b) const { return a < b ? this->sum(b) - this->sum(a) : 0; }
+
+    /**
+     * @brief binary search on fenwick_tree
+     *
+     * @param val target value
+     * @return int
+     */
+    int lower_bound(T val) const {
+        if (val <= 0) return 0;
+        int k = 1;
+        while (k < this->_size) k <<= 1;
+        int res = 0;
+        for (; k > 0; k >>= 1) {
+            if (res + k < this->_size && this->data[res + k] < val) val -= this->data[res += k];
+        }
+        return res;
+    }
+
+  private:
+    int _size;
+    std::vector<T> data;
+};
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 2 "/home/kuhaku/home/github/algo/lib/string/converter.hpp"
+
+struct string_converter {
+    char type(const char &c) const {
+        return (std::islower(c) ? 'a' : std::isupper(c) ? 'A' : std::isdigit(c) ? '0' : 0);
+    }
+    int convert(const char &c) {
+        if (!start) start = type(c);
+        return c - start;
+    }
+    int convert(const char &c, const std::string &chars) { return chars.find(c); }
+    template <typename T>
+    auto convert(const T &v) {
+        std::vector<decltype(convert(v[0]))> res;
+        res.reserve(v.size());
+        for (auto &&e : v) res.emplace_back(convert(e));
+        return res;
+    }
+    template <typename T>
+    auto convert(const T &v, const std::string &chars) {
+        std::vector<decltype(convert(v[0], chars))> res;
+        res.reserve(v.size());
+        for (auto &&e : v) res.emplace_back(convert(e, chars));
+        return res;
+    }
+    int operator()(const char &v, char s = 0) {
+        start = s;
+        return convert(v);
+    }
+    int operator()(const char &v, const std::string &chars) { return convert(v, chars); }
+    template <typename T>
+    auto operator()(const T &v, char s = 0) {
+        start = s;
+        return convert(v);
+    }
+    template <typename T>
+    auto operator()(const T &v, const std::string &chars) {
+        return convert(v, chars);
+    }
+
+  private:
+    char start = 0;
+} to_int;
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 5 "a.cpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    string t;
+    cin >> t;
+    vector<string> s(n);
+    cin >> s;
+    // auto u = to_int(t, 'a');
+    // vector<vector<int>> v(n);
+    // rep (i, n) v[i] = to_int(s[i], 'a');
+
+    // int m = t.size();
+    // vector<vector<int>> a(26);
+    // rep (i, m) {
+    //     a[u[i]].emplace_back(i);
+    // }
+
+    int m = t.size();
+    vector<int> ls(n), rs(n);
+    rep (i, n) {
+        int l = 0;
+        for (auto c : s[i]) {
+            if (l < m && c == t[l])
+                ++l;
+        }
+        ls[i] = l;
+    }
+    rep (i, n) {
+        int r = m - 1;
+        reverse(all(s[i]));
+        for (auto c : s[i]) {
+            if (r >= 0 && c == t[r])
+                --r;
+        }
+        rs[i] = r;
+    }
+
+    ll ans = 0;
+    fenwick_tree<int> ft(m + 1);
+    rep (i, n) {
+        ft.add(rs[i] + 1, 1);
+    }
+    rep (i, n) ans += ft.sum(ls[i] + 1);
+    co(ans);
+
+    return 0;
+}
---
+++ code/abc/abc324/abc324_f.cpp
@@ -0,0 +1,253 @@
+#line 2 "/home/kuhaku/home/github/algo/lib/graph/graph.hpp"
+#include <iostream>
+#include <vector>
+
+/**
+ * @brief 重み付きグラフ
+ *
+ * @tparam T 辺の重みの型
+ */
+template <class T>
+struct Graph {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to(), _weight() {}
+        constexpr _edge(int from, int to, T weight) : _from(from), _to(to), _weight(weight) {}
+        constexpr bool operator<(const _edge &rhs) const { return weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+        constexpr int from() const { return _from; }
+        constexpr int to() const { return _to; }
+        constexpr T weight() const { return _weight; }
+
+      private:
+        int _from, _to;
+        T _weight;
+    };
+
+  public:
+    using edge_type = typename Graph<T>::_edge;
+
+    Graph() : _size(), edges() {}
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+    constexpr int size() const { return _size; }
+
+    void add_edge(const edge_type &e) { edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to, T weight = T(1)) { edges[from].emplace_back(from, to, weight); }
+    void add_edges(int from, int to, T weight = T(1)) {
+        edges[from].emplace_back(from, to, weight);
+        edges[to].emplace_back(to, from, weight);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            std::cin >> from >> to >> weight;
+            add_edge(from - base, to - base, weight);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            std::cin >> from >> to >> weight;
+            add_edges(from - base, to - base, weight);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+
+template <>
+struct Graph<void> {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to() {}
+        constexpr _edge(int from, int to) : _from(from), _to(to) {}
+
+        constexpr int from() const { return _from; }
+        constexpr int to() const { return _to; }
+        constexpr int weight() const { return 1; }
+        constexpr bool operator<(const _edge &rhs) const { return weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+      private:
+        int _from, _to;
+    };
+
+  public:
+    using edge_type = typename Graph<void>::_edge;
+
+    Graph() : _size(), edges() {}
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+    constexpr int size() const { return _size; }
+
+    void add_edge(const edge_type &e) { edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to) { edges[from].emplace_back(from, to); }
+    void add_edges(int from, int to) {
+        edges[from].emplace_back(from, to);
+        edges[to].emplace_back(to, from);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            std::cin >> from >> to;
+            add_edge(from - base, to - base);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            std::cin >> from >> to;
+            add_edges(from - base, to - base);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 3 "a.cpp"
+
+struct S {
+    int a, b;
+
+    friend std::istream &operator>>(std::istream &is, S &s) {
+        is >> s.a >> s.b;
+        return is;
+    }
+};
+
+int main(void) {
+    int n, m;
+    cin >> n >> m;
+    Graph<S> g(n);
+    g.input_edges(m);
+
+    double l = 0, r = INF;
+    while (r - l > 1e-9) {
+        double m = (l + r) / 2;
+        vector<double> dp(n, -INF);
+        dp[0] = 0;
+        rep (i, n) {
+            for (auto e : g[i]) {
+                chmax(dp[e.to()], dp[i] + e.weight().a - e.weight().b * m);
+            }
+        }
+        (dp[n - 1] >= 0 ? l : r) = m;
+    }
+    co(l);
+
+    return 0;
+}

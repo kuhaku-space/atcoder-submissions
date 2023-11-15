cf2fda6968eaf1dc16790c06010342e16321e94b
GitHub <noreply@github.com>
2023-11-15 23:19:05 +0000

    [auto-commit] Add my codes

--- code/abc/abc290/abc290_c.cpp
+++ code/abc/abc290/abc290_c.cpp
@@ -1,66 +1,67 @@
 #line 1 "a.cpp"
 #define PROBLEM ""
-#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
-#pragma GCC target("sse4.2,avx2,bmi2")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h>
-template <class T, class U>
-bool chmax(T &a, const U &b) {
-    return a < (T)b ? a = (T)b, true : false;
-}
-template <class T, class U>
-bool chmin(T &a, const U &b) {
-    return (T)b < a ? a = (T)b, true : false;
-}
-constexpr std::int64_t INF = 1000000000000000003;
-constexpr int Inf = 1000000003;
-constexpr int MOD = 1000000007;
-constexpr int MOD_N = 998244353;
-constexpr double EPS = 1e-7;
-constexpr double PI = M_PI;
-#line 2 "/home/kuhaku/home/github/algo/lib/algorithm/mex.hpp"
+#line 1 "/home/kuhaku/home/github/algo/lib/algorithm/mex.hpp"
+#include <algorithm>
+#include <iterator>
+#include <vector>
 
 /**
  * @brief Mex
  *
  */
 struct minimum_excluded {
-    minimum_excluded() : n(), _size(), is_exist(64), v() {}
+    minimum_excluded() : n(), _size(), exists(64), v() {}
 
-    constexpr int operator()() const noexcept { return this->n; }
-    constexpr int get() const noexcept { return this->n; }
+    constexpr int operator()() const noexcept { return n; }
+    constexpr int get() const noexcept { return n; }
 
     void add(int x) {
         if (x < 0) return;
-        ++this->_size;
-        if (this->_size == (int)this->is_exist.size()) {
-            this->is_exist.resize(this->_size << 1);
-            int cnt = 0;
-            for (int i = 0; i < (int)this->v.size(); ++i) {
-                if (this->v[i] < (int)this->is_exist.size()) {
-                    if (this->is_exist[this->v[i]]) --this->_size;
-                    else this->is_exist[this->v[i]] = true;
-                } else {
-                    this->v[cnt++] = this->v[i];
-                }
-            }
-            this->v.erase(this->v.begin() + cnt, this->v.end());
+        ++_size;
+        if (_size == (int)exists.size()) {
+            exists.resize(_size << 1);
+            v.erase(std::remove_if(v.begin(), v.end(),
+                                   [&](int y) {
+                                       if (y < (int)exists.size()) {
+                                           if (exists[y]) --_size;
+                                           else exists[y] = true;
+                                           return true;
+                                       }
+                                       return false;
+                                   }),
+                    v.end());
         }
-        if (x < (int)this->is_exist.size()) {
-            if (this->is_exist[x]) --this->_size;
-            else this->is_exist[x] = true;
+        if (x < (int)exists.size()) {
+            if (exists[x]) --_size;
+            else exists[x] = true;
         } else {
-            this->v.emplace_back(x);
+            v.emplace_back(x);
         }
-        while (this->is_exist[this->n]) ++this->n;
+        while (exists[n]) ++n;
     }
 
   private:
     int n, _size;
-    std::vector<bool> is_exist;
+    std::vector<bool> exists;
     std::vector<int> v;
 };
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
 #line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
 #define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
 #define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
@@ -75,6 +76,7 @@ struct Sonic {
     Sonic() {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
     }
 
     constexpr void operator()() const {}
@@ -117,9 +119,6 @@ template <typename T, typename... Args>
 auto make_vector(T x, int arg, Args... args) {
     if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
     else return std::vector(arg, make_vector<T>(x, args...));
-}
-void setp(int n) {
-    std::cout << std::fixed << std::setprecision(n);
 }
 void Yes(bool is_correct = true) {
     std::cout << (is_correct ? "Yes" : "No") << '\n';

e82eec5124725dd3cd9000ba199712955e8c2932
GitHub <noreply@github.com>
2023-12-03 23:18:23 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc331/abc331_e.cpp
@@ -0,0 +1,138 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 3 "a.cpp"
+
+int main(void) {
+    int n, m, l;
+    cin >> n >> m >> l;
+    vector<ll> a(n), b(m);
+    cin >> a >> b;
+    vector<pair<int, int>> c(l);
+    cin >> c;
+    set<pair<int, int>> st;
+    for (auto [x, y] : c) st.emplace(x - 1, y - 1);
+
+    vector<int> ord_a(n), ord_b(m);
+    iota(all(ord_a), 0);
+    iota(all(ord_b), 0);
+    sort(all(ord_a), [&](int x, int y) {
+        return a[x] > a[y];
+    });
+    sort(all(ord_b), [&](int x, int y) {
+        return b[x] > b[y];
+    });
+    priority_queue<tuple<ll, int, int>> pq;
+    pq.emplace(a[ord_a[0]] + b[ord_b[0]], 0, 0);
+    set<pair<int, int>> s;
+    while (true) {
+        auto [v, x, y] = pq.top();
+        pq.pop();
+        if (!st.count({ord_a[x], ord_b[y]})) {
+            co(v);
+            break;
+        }
+        if (x + 1 < n && !s.count({x + 1, y})) {
+            pq.emplace(a[ord_a[x + 1]] + b[ord_b[y]], x + 1, y);
+            s.emplace(x + 1, y);
+        }
+        if (y + 1 < m && !s.count({x, y + 1})) {
+            pq.emplace(a[ord_a[x]] + b[ord_b[y + 1]], x, y + 1);
+            s.emplace(x, y + 1);
+        }
+    }
+
+    return 0;
+}

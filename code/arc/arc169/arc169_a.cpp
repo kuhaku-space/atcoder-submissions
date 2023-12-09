135c01cd431bac08dd6ee88c63c08ab19dc6f396
GitHub <noreply@github.com>
2023-12-09 23:18:08 +0000

    [auto-commit] Add my codes

---
+++ code/arc/arc169/arc169_a.cpp
@@ -0,0 +1,378 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/graph/graph.hpp"
+#include <iostream>
+#include <vector>
+
+/**
+ * @brief 重み付きグラフ
+ *
+ * @tparam T 辺の重みの型
+ */
+template <class T>
+struct Graph {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to(), _weight() {}
+        constexpr _edge(int from, int to, T weight) : _from(from), _to(to), _weight(weight) {}
+        constexpr bool operator<(const _edge &rhs) const { return weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+        constexpr int from() const { return _from; }
+        constexpr int to() const { return _to; }
+        constexpr T weight() const { return _weight; }
+
+      private:
+        int _from, _to;
+        T _weight;
+    };
+
+  public:
+    using edge_type = typename Graph<T>::_edge;
+
+    Graph() : _size(), edges() {}
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+    constexpr int size() const { return _size; }
+
+    void add_edge(const edge_type &e) { edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to, T weight = T(1)) { edges[from].emplace_back(from, to, weight); }
+    void add_edges(int from, int to, T weight = T(1)) {
+        edges[from].emplace_back(from, to, weight);
+        edges[to].emplace_back(to, from, weight);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            std::cin >> from >> to >> weight;
+            add_edge(from - base, to - base, weight);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            std::cin >> from >> to >> weight;
+            add_edges(from - base, to - base, weight);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+
+template <>
+struct Graph<void> {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to() {}
+        constexpr _edge(int from, int to) : _from(from), _to(to) {}
+
+        constexpr int from() const { return _from; }
+        constexpr int to() const { return _to; }
+        constexpr int weight() const { return 1; }
+        constexpr bool operator<(const _edge &rhs) const { return weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+      private:
+        int _from, _to;
+    };
+
+  public:
+    using edge_type = typename Graph<void>::_edge;
+
+    Graph() : _size(), edges() {}
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+    constexpr int size() const { return _size; }
+
+    void add_edge(const edge_type &e) { edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to) { edges[from].emplace_back(from, to); }
+    void add_edges(int from, int to) {
+        edges[from].emplace_back(from, to);
+        edges[to].emplace_back(to, from);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            std::cin >> from >> to;
+            add_edge(from - base, to - base);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            std::cin >> from >> to;
+            add_edges(from - base, to - base);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 3 "/home/kuhaku/home/github/algo/lib/tree/tree_function.hpp"
+
+template <class T>
+std::vector<int> tree_bfs(const Graph<T> &g, int r = 0) {
+    int pos = 0;
+    std::vector<int> res;
+    std::vector<bool> visited(g.size());
+    res.emplace_back(r);
+    visited[r] = true;
+    while (pos < (int)res.size()) {
+        auto index = res[pos++];
+        for (auto &e : g[index]) {
+            if (visited[e.to()]) continue;
+            res.emplace_back(e.to());
+            visited[e.to()] = true;
+        }
+    }
+    return res;
+}
+
+std::vector<int> tree_bfs(const std::vector<int> &parents) {
+    int n = parents.size();
+    Graph<void> g(n);
+    int r = 0;
+    for (int i = 0; i < n; ++i) {
+        if (parents[i] == -1 || parents[i] == i) {
+            r = i;
+            continue;
+        }
+        g.add_edges(i, parents[i]);
+    }
+    return tree_bfs(g, r);
+}
+
+template <class T>
+std::vector<int> tree_dfs(const Graph<T> &g, int r = 0) {
+    std::vector<int> res;
+    auto dfs = [&g, &res](auto self, int index, int parent) {
+        res.emplace_back(index);
+        for (auto &e : g[index]) {
+            if (e.to() == parent) continue;
+            self(self, e.to(), index);
+        }
+    };
+    dfs(dfs, r, -1);
+    return res;
+}
+
+/**
+ * @brief 木の距離を求める
+ *
+ * @tparam T 辺の重みの型
+ * @tparam U 距離の型
+ * @param g 木
+ * @param r 根
+ * @return std::vector<U> 各頂点の根からの距離
+ */
+template <class T, class U = T>
+std::vector<U> tree_dist(const Graph<T> &g, int r = 0) {
+    std::vector<U> res(g.size(), -1);
+    std::stack<int> st;
+    res[r] = 0;
+    st.emplace(r);
+    while (!st.empty()) {
+        auto index = st.top();
+        st.pop();
+        for (auto &e : g[index]) {
+            if (res[e.to()] != -1) continue;
+            res[e.to()] = res[index] + e.weight();
+            st.emplace(e.to());
+        }
+    }
+    return res;
+}
+
+/**
+ * @brief 木の頂点の親を求める
+ *
+ * @tparam T 辺の重みの型
+ * @param g 木
+ * @param r 根
+ * @return std::vector<int>
+ */
+template <class T>
+std::vector<int> tree_parent(const Graph<T> &g, int r = 0) {
+    std::vector<int> res(g.size(), -1);
+    std::stack<int> st;
+    res[r] = r;
+    st.emplace(r);
+    while (!st.empty()) {
+        auto index = st.top();
+        st.pop();
+        for (auto &e : g[index]) {
+            if (res[e.to()] != -1) continue;
+            res[e.to()] = index;
+            st.emplace(e.to());
+        }
+    }
+    res[r] = -1;
+    return res;
+}
+
+/**
+ * @brief 部分木の大きさを求める
+ *
+ * @tparam T 辺の重みの型
+ * @param g グラフ
+ * @param r 根
+ * @return std::vector<int>
+ */
+template <class T>
+std::vector<int> tree_subtree(const Graph<T> &g, int r = 0) {
+    std::vector<int> res(g.size());
+    auto dfs = [&g, &res](auto self, int index) -> int {
+        res[index] = 1;
+        for (auto &e : g[index]) {
+            if (res[e.to()] != 0) continue;
+            res[index] += self(self, e.to());
+        }
+        return res[index];
+    };
+    dfs(dfs, r);
+    return res;
+}
+#line 5 "a.cpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    cin >> a;
+    vector<int> p(n - 1);
+    cin >> p;
+    Graph<void> g(n);
+    rep (i, n - 1) {
+        g.add_edges(i + 1, p[i] - 1);
+    }
+    auto dep = tree_dist<void, int>(g);
+    vector<ll> s(n + 1);
+    rep (i, n) {
+        s[dep[i]] += a[i];
+    }
+    repr (i, n + 1) {
+        if (s[i] != 0) {
+            if (s[i] > 0) {
+                co("+");
+            } else {
+                co("-");
+            }
+            return 0;
+        }
+    }
+    co(0);
+
+    return 0;
+}

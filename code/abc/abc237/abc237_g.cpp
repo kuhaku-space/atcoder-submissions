0483ba0f537a527a7fa272cc6b5e39976f7287dc
GitHub <noreply@github.com>
2023-12-17 23:18:56 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc237/abc237_g.cpp
@@ -0,0 +1,410 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/internal/internal_bit.hpp"
+
+namespace internal {
+
+// @return same with std::bit::bit_ceil
+unsigned int bit_ceil(unsigned int n) {
+    unsigned int x = 1;
+    while (x < (unsigned int)(n)) x *= 2;
+    return x;
+}
+
+// @param n `1 <= n`
+// @return same with std::bit::countr_zero
+int countr_zero(unsigned int n) { return __builtin_ctz(n); }
+
+// @param n `1 <= n`
+// @return same with std::bit::countr_zero
+constexpr int countr_zero_constexpr(unsigned int n) {
+    int x = 0;
+    while (!(n & (1 << x))) x++;
+    return x;
+}
+
+}  // namespace internal
+#line 2 "/home/kuhaku/home/github/algo/lib/segment_tree/monoid.hpp"
+#include <algorithm>
+#include <limits>
+#include <utility>
+
+template <class T>
+struct Add {
+    using value_type = T;
+    static constexpr T id = T(0);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs + rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs + rhs;
+    }
+};
+
+template <class T>
+struct And {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs & rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs & rhs;
+    }
+};
+
+template <class T>
+struct Or {
+    using value_type = T;
+    static constexpr T id = T(0);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs | rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs | rhs;
+    }
+};
+
+template <class T>
+struct Xor {
+    using value_type = T;
+    static constexpr T id = T(0);
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs ^ rhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs ^ rhs;
+    }
+};
+
+template <class T>
+struct Min {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) { return std::min(lhs, rhs); }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return std::min((U)lhs, rhs);
+    }
+};
+
+template <class T>
+struct Max {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::min();
+    static constexpr T op(const T &lhs, const T &rhs) { return std::max(lhs, rhs); }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return std::max((U)lhs, rhs);
+    }
+};
+
+template <class T>
+struct Update {
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) { return lhs == Update::id ? rhs : lhs; }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs == Update::id ? rhs : lhs;
+    }
+};
+
+template <class T>
+struct Affine {
+    using value_type = std::pair<T, T>;
+    static constexpr std::pair<T, T> id = std::pair<T, T>(1, 0);
+    static constexpr std::pair<T, T> op(std::pair<T, T> lhs, std::pair<T, T> rhs) {
+        return {lhs.first * rhs.first, lhs.first * rhs.second + lhs.second};
+    }
+};
+
+template <class M>
+struct Rev {
+    using T = typename M::value_type;
+    using value_type = T;
+    static constexpr T id = M::id;
+    static constexpr T op(T lhs, T rhs) { return M::op(rhs, lhs); }
+};
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 5 "/home/kuhaku/home/github/algo/lib/segment_tree/lazy_segment_tree.hpp"
+
+/**
+ * @brief 遅延評価セグメント木
+ * @see https://github.com/atcoder/ac-library/blob/master/atcoder/lazysegtree.hpp
+ *
+ * @tparam S モノイド
+ * @tparam F モノイド
+ */
+template <class S, class F>
+struct lazy_segment_tree {
+  private:
+    using T = typename S::value_type;
+    using U = typename F::value_type;
+
+  public:
+    lazy_segment_tree() : lazy_segment_tree(0) {}
+    explicit lazy_segment_tree(int n, T e = S::id) : lazy_segment_tree(std::vector<T>(n, e)) {}
+    explicit lazy_segment_tree(const std::vector<T> &v) : _n(int(v.size())) {
+        _size = internal::bit_ceil(_n);
+        _log = internal::countr_zero(_size);
+        data = std::vector<T>(2 * _size, S::id);
+        lazy = std::vector<U>(_size, F::id);
+        for (int i = 0; i < _n; i++) data[_size + i] = v[i];
+        for (int i = _size - 1; i >= 1; --i) update(i);
+    }
+
+    void set(int p, T x) {
+        assert(0 <= p && p < _n);
+        p += _size;
+        for (int i = _log; i >= 1; --i) push(p >> i);
+        data[p] = x;
+        for (int i = 1; i <= _log; ++i) update(p >> i);
+    }
+
+    T at(int p) { return get(p); }
+    T get(int p) {
+        assert(0 <= p && p < _n);
+        p += _size;
+        for (int i = _log; i >= 1; --i) push(p >> i);
+        return data[p];
+    }
+
+    void apply(int p, U f) {
+        assert(0 <= p && p < _n);
+        p += _size;
+        for (int i = _log; i >= 1; --i) push(p >> i);
+        data[p] = F::f(f, data[p]);
+        for (int i = 1; i <= _log; ++i) update(p >> i);
+    }
+    void apply(int l, int r, U f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+
+        l += _size, r += _size;
+
+        for (int i = _log; i >= 1; --i) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        int l2 = l, r2 = r;
+        while (l < r) {
+            if (l & 1) all_apply(l++, f);
+            if (r & 1) all_apply(--r, f);
+            l >>= 1, r >>= 1;
+        }
+        l = l2, r = r2;
+
+        for (int i = 1; i <= _log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+
+    T prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return S::id;
+
+        l += _size, r += _size;
+
+        for (int i = _log; i >= 1; --i) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        T sml = S::id, smr = S::id;
+        while (l < r) {
+            if (l & 1) sml = S::op(sml, data[l++]);
+            if (r & 1) smr = S::op(data[--r], smr);
+            l >>= 1, r >>= 1;
+        }
+
+        return S::op(sml, smr);
+    }
+
+    T all_prod() const { return data[1]; }
+
+  private:
+    int _n, _size, _log;
+    std::vector<T> data;
+    std::vector<U> lazy;
+
+    void update(int k) { data[k] = S::op(data[2 * k], data[2 * k + 1]); }
+    void all_apply(int k, U f) {
+        data[k] = F::f(f, data[k]);
+        if (k < _size) lazy[k] = F::op(f, lazy[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lazy[k]);
+        all_apply(2 * k + 1, lazy[k]);
+        lazy[k] = F::id;
+    }
+};
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 4 "a.cpp"
+
+struct M {
+    using T = pair<int, int>;
+    using value_type = T;
+    static constexpr T id = T(0, 0);
+    static constexpr T op(const T &lhs, const T &rhs) {
+        return {lhs.first + rhs.first, lhs.second + rhs.second};
+    }
+};
+
+struct F {
+    using T = int;
+    using value_type = T;
+    static constexpr T id = std::numeric_limits<T>::max();
+    static constexpr T op(const T &lhs, const T &rhs) {
+        return lhs == F::id ? rhs : lhs;
+    }
+
+    template <class U>
+    static constexpr U f(T lhs, U rhs) {
+        return lhs == F::id ? rhs : make_pair(lhs * rhs.second, rhs.second);
+    }
+};
+
+int main(void) {
+    int n, q, x;
+    cin >> n >> q >> x;
+    vector<int> a(n);
+    cin >> a;
+    lazy_segment_tree<M, F> lst1(n), lst2(n);
+    rep (i, n) {
+        if (a[i] < x)
+            lst1.set(i, {0, 1});
+        else
+            lst1.set(i, {1, 1});
+        if (a[i] <= x)
+            lst2.set(i, {0, 1});
+        else
+            lst2.set(i, {1, 1});
+    }
+    while (q--) {
+        int c, l, r;
+        cin >> c >> l >> r;
+        --l;
+        auto p = lst1.prod(l, r);
+        auto q = lst2.prod(l, r);
+        if (c == 1) {
+            int x = p.second - p.first;
+            lst1.apply(l, l + x, 0);
+            lst1.apply(l + x, r, 1);
+            int y = q.second - q.first;
+            lst2.apply(l, l + y, 0);
+            lst2.apply(l + y, r, 1);
+        } else {
+            int x = p.first;
+            lst1.apply(l, l + x, 1);
+            lst1.apply(l + x, r, 0);
+            int y = q.first;
+            lst2.apply(l, l + y, 1);
+            lst2.apply(l + y, r, 0);
+        }
+    }
+
+    rep (i, n) {
+        if (lst1.get(i).first != lst2.get(i).first) {
+            co(i + 1);
+        }
+    }
+
+    return 0;
+}
---
+++ code/abc/abc241/abc241_g.cpp
@@ -0,0 +1,327 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 2 "/home/kuhaku/home/github/algo/lib/flow/max_flow.hpp"
+
+/**
+ * @brief 最大流
+ *
+ * @tparam Cap
+ */
+template <class Cap>
+struct mf_graph {
+    mf_graph() : _n(0) {}
+    explicit mf_graph(int n) : _n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = int(pos.size());
+        pos.emplace_back(from, int(g[from].size()));
+        int from_id = int(g[from].size());
+        int to_id = int(g[to].size());
+        if (from == to) ++to_id;
+        g[from].emplace_back(to, to_id, cap);
+        g[to].emplace_back(from, from_id, 0);
+        return m;
+    }
+
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    edge get_edge(int i) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return edge{pos[i].first, _e.to, _e.cap + _re.cap, _re.cap};
+    }
+    std::vector<edge> edges() {
+        int m = int(pos.size());
+        std::vector<edge> result;
+        for (int i = 0; i < m; ++i) {
+            result.emplace_back(get_edge(i));
+        }
+        return result;
+    }
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto &_e = g[pos[i].first][pos[i].second];
+        auto &_re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) { return flow(s, t, std::numeric_limits<Cap>::max()); }
+    Cap flow(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        std::vector<int> level(_n), iter(_n);
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            std::queue<int> que;
+            que.emplace(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.emplace(e.to);
+                }
+            }
+        };
+        auto dfs = [&](auto self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int &i = iter[v]; i < int(g[v].size()); ++i) {
+                _edge &e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d = self(self, e.to, std::min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) return res;
+            }
+            level[v] = _n;
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit - flow);
+            if (!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(_n);
+        std::queue<int> que;
+        que.emplace(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.emplace(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private:
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+
+        constexpr _edge(int _to, int _rev, Cap _cap) : to(_to), rev(_rev), cap(_cap) {}
+    };
+    std::vector<std::pair<int, int>> pos;
+    std::vector<std::vector<_edge>> g;
+};
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 3 "/home/kuhaku/home/github/algo/lib/template/vector.hpp"
+
+struct increment_impl {
+    template <class T>
+    const increment_impl &operator>>(std::vector<T> &v) const {
+        for (auto &x : v) ++x;
+        return *this;
+    }
+} Inc;
+
+struct decrement_impl {
+    template <class T>
+    const decrement_impl &operator>>(std::vector<T> &v) const {
+        for (auto &x : v) --x;
+        return *this;
+    }
+} Dec;
+
+struct sort_impl {
+    template <class T>
+    const sort_impl &operator>>(std::vector<T> &v) const {
+        std::sort(v.begin(), v.end());
+        return *this;
+    }
+} Sort;
+
+struct unique_impl {
+    template <class T>
+    const unique_impl &operator>>(std::vector<T> &v) const {
+        std::sort(v.begin(), v.end());
+        v.erase(std::unique(v.begin(), v.end()), v.end());
+        return *this;
+    }
+} Uniq;
+#line 5 "a.cpp"
+
+int main(void) {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> p(m);
+    cin >> p;
+    vector<vector<int>> v(n, vector(n, 0));
+    for (auto [x, y] : p) {
+        --x, --y;
+        v[x][y] = 1;
+        v[y][x] = -1;
+    }
+
+    vector<int> ans;
+    rep (i, n) {
+        auto u = v;
+        rep (j, n) {
+            if (i == j)
+                continue;
+            if (u[i][j] == 0) {
+                u[i][j] = 1;
+                u[j][i] = -1;
+            }
+        }
+
+        int m = 0;
+        rep (j, n) {
+            m += u[i][j] == 1;
+        }
+        if (m == 0)
+            continue;
+        mf_graph<int> mf(n * (n - 1) / 2 + n + 2);
+        int s = n * (n - 1) / 2 + n, t = s + 1;
+        rep (j, n * (n - 1) / 2) mf.add_edge(s, j, 1);
+        rep (j, n) {
+            if (j == i)
+                mf.add_edge(n * (n - 1) / 2 + j, t, m);
+            else
+                mf.add_edge(n * (n - 1) / 2 + j, t, m - 1);
+        }
+
+        int k = 0;
+        rep (x, n) {
+            rep (y, x) {
+                if (u[x][y] != -1)
+                    mf.add_edge(k, n * (n - 1) / 2 + x, 1);
+                if (u[y][x] != -1)
+                    mf.add_edge(k, n * (n - 1) / 2 + y, 1);
+                ++k;
+            }
+        }
+        if (mf.flow(s, t) == n * (n - 1) / 2)
+            ans.emplace_back(i);
+    }
+    Inc >> ans;
+    co(ans);
+
+    return 0;
+}

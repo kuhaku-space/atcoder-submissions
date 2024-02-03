ea6215ffe96ffdb7aa08c318425f286196749256
GitHub <noreply@github.com>
2024-02-03 23:18:37 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc339/abc339_d.cpp
@@ -0,0 +1,338 @@
+#line 1 "a.cpp"
+#define PROBLEM ""
+#line 2 "/home/kuhaku/home/github/algo/lib/graph/graph.hpp"
+#include <iostream>
+#include <vector>
+
+/**
+ * @brief 重み付きグラフ
+ *
+ * @tparam T 辺の重みの型
+ */
+template <class T>
+struct Graph {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to(), _weight() {}
+        constexpr _edge(int from, int to, T weight) : _from(from), _to(to), _weight(weight) {}
+        constexpr bool operator<(const _edge &rhs) const { return weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+        constexpr int from() const { return _from; }
+        constexpr int to() const { return _to; }
+        constexpr T weight() const { return _weight; }
+
+      private:
+        int _from, _to;
+        T _weight;
+    };
+
+  public:
+    using edge_type = typename Graph<T>::_edge;
+
+    Graph() : _size(), edges() {}
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+    constexpr int size() const { return _size; }
+
+    void add_edge(const edge_type &e) { edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to, T weight = T(1)) { edges[from].emplace_back(from, to, weight); }
+    void add_edges(int from, int to, T weight = T(1)) {
+        edges[from].emplace_back(from, to, weight);
+        edges[to].emplace_back(to, from, weight);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            std::cin >> from >> to >> weight;
+            add_edge(from - base, to - base, weight);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            T weight;
+            std::cin >> from >> to >> weight;
+            add_edges(from - base, to - base, weight);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+
+template <>
+struct Graph<void> {
+  private:
+    struct _edge {
+        constexpr _edge() : _from(), _to() {}
+        constexpr _edge(int from, int to) : _from(from), _to(to) {}
+
+        constexpr int from() const { return _from; }
+        constexpr int to() const { return _to; }
+        constexpr int weight() const { return 1; }
+        constexpr bool operator<(const _edge &rhs) const { return weight() < rhs.weight(); }
+        constexpr bool operator>(const _edge &rhs) const { return rhs < *this; }
+
+      private:
+        int _from, _to;
+    };
+
+  public:
+    using edge_type = typename Graph<void>::_edge;
+
+    Graph() : _size(), edges() {}
+    Graph(int v) : _size(v), edges(v) {}
+
+    const auto &operator[](int i) const { return edges[i]; }
+    auto &operator[](int i) { return edges[i]; }
+    const auto begin() const { return edges.begin(); }
+    auto begin() { return edges.begin(); }
+    const auto end() const { return edges.end(); }
+    auto end() { return edges.end(); }
+    constexpr int size() const { return _size; }
+
+    void add_edge(const edge_type &e) { edges[e.from()].emplace_back(e); }
+    void add_edge(int from, int to) { edges[from].emplace_back(from, to); }
+    void add_edges(int from, int to) {
+        edges[from].emplace_back(from, to);
+        edges[to].emplace_back(to, from);
+    }
+
+    void input_edge(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            std::cin >> from >> to;
+            add_edge(from - base, to - base);
+        }
+    }
+    void input_edges(int m, int base = 1) {
+        for (int i = 0; i < m; ++i) {
+            int from, to;
+            std::cin >> from >> to;
+            add_edges(from - base, to - base);
+        }
+    }
+
+  private:
+    int _size;
+    std::vector<std::vector<edge_type>> edges;
+};
+#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
+#pragma GCC target("sse4.2,avx2,bmi2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = M_PI;
+#line 4 "/home/kuhaku/home/github/algo/lib/graph/dijkstra.hpp"
+
+/**
+ * @brief ダイクストラ法
+ *
+ * @tparam T 辺の重みの型
+ * @param g グラフ
+ * @param s 始点
+ * @param inf 正の無限表現
+ * @retval std::vector<T> 各頂点までの最短距離
+ */
+template <class T>
+std::vector<T> dijkstra(const Graph<T> &g, int s = 0, T inf = std::numeric_limits<T>::max()) {
+    struct _node {
+        constexpr _node() : _to(), _dist() {}
+        constexpr _node(int to, T dist) : _to(to), _dist(dist) {}
+        constexpr bool operator<(const _node &rhs) const { return this->dist() < rhs.dist(); }
+        constexpr bool operator>(const _node &rhs) const { return rhs < *this; }
+
+        constexpr int to() const { return this->_to; }
+        constexpr T dist() const { return this->_dist; }
+
+      private:
+        int _to;
+        T _dist;
+    };
+    std::vector<T> dists(g.size(), inf);
+    std::priority_queue<_node, std::vector<_node>, std::greater<>> p_que;
+    dists[s] = T();
+    p_que.emplace(s, T());
+    while (!p_que.empty()) {
+        auto node = p_que.top();
+        p_que.pop();
+        if (dists[node.to()] < node.dist()) continue;
+        for (auto &e : g[node.to()]) {
+            if (chmin(dists[e.to()], node.dist() + e.weight()))
+                p_que.emplace(e.to(), node.dist() + e.weight());
+        }
+    }
+    return dists;
+}
+
+std::vector<int> dijkstra(const Graph<void> &g, int s = 0,
+                          int inf = std::numeric_limits<int>::max()) {
+    std::vector<int> dists(g.size(), inf);
+    std::queue<int> que;
+    dists[s] = 0;
+    que.emplace(s);
+    while (!que.empty()) {
+        auto index = que.front();
+        que.pop();
+        for (auto &e : g[index]) {
+            if (chmin(dists[e.to()], dists[index] + 1)) que.emplace(e.to());
+        }
+    }
+    return dists;
+}
+#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+
+    constexpr void operator()() const {}
+} sonic;
+#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << (it == v.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+template <typename T, typename... Args>
+auto make_vector(T x, int arg, Args... args) {
+    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
+    else return std::vector(arg, make_vector<T>(x, args...));
+}
+void Yes(bool is_correct = true) {
+    std::cout << (is_correct ? "Yes" : "No") << '\n';
+}
+void No(bool is_not_correct = true) {
+    Yes(!is_not_correct);
+}
+void YES(bool is_correct = true) {
+    std::cout << (is_correct ? "YES" : "NO") << '\n';
+}
+void NO(bool is_not_correct = true) {
+    YES(!is_not_correct);
+}
+void Takahashi(bool is_correct = true) {
+    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
+}
+void Aoki(bool is_not_correct = true) {
+    Takahashi(!is_not_correct);
+}
+#line 4 "a.cpp"
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    cin >> a;
+    vector<pair<int, int>> s;
+    rep (i, n) {
+        rep (j, n) {
+            if (a[i][j] == 'P')
+                s.emplace_back(i, j);
+        }
+    }
+
+    auto in_field = [&](int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    };
+    auto flatten = [&](pair<int, int> p) {
+        return p.first * n + p.second;
+    };
+
+    vector nxt(n * n, vector(4, 0));
+    vector<int> dx = {1, -1, 0, 0};
+    vector<int> dy = {0, 0, 1, -1};
+    rep (i, n * n) {
+        int x = i / n;
+        int y = i % n;
+        rep (k, 4) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if (in_field(nx, ny) && a[nx][ny] != '#') {
+                nxt[i][k] = nx * n + ny;
+            } else {
+                nxt[i][k] = i;
+            }
+        }
+    }
+
+    Graph<void> g(n * n * n * n);
+    rep (i, n * n) {
+        rep (j, n * n) {
+            rep (k, 4) {
+                g.add_edge(i * n * n + j, nxt[i][k] * n * n + nxt[j][k]);
+            }
+        }
+    }
+
+    auto dist = dijkstra(g, flatten(s[0]) * n * n + flatten(s[1]));
+    int ans = Inf;
+    rep (i, n * n) {
+        chmin(ans, dist[i * n * n + i]);
+    }
+    if (ans == Inf)
+        ans = -1;
+    co(ans);
+
+    return 0;
+}

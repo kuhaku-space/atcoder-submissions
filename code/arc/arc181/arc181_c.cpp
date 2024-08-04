3e0a136d588a31495b7f6d438c4b3cd5555ea205
GitHub <noreply@github.com>
2024-08-04 23:22:22 +0000

    [auto-commit] Add my codes

---
+++ code/arc/arc181/arc181_c.cpp
@@ -0,0 +1,142 @@
+// competitive-verifier: PROBLEM
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = 3.14159265358979323846;
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+    constexpr void operator()() const {}
+} sonic;
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
+struct increment_impl {
+    template <class T>
+    const increment_impl &operator>>(std::vector<T> &v) const {
+        for (auto &x : v) ++x;
+        return *this;
+    }
+} Inc;
+struct decrement_impl {
+    template <class T>
+    const decrement_impl &operator>>(std::vector<T> &v) const {
+        for (auto &x : v) --x;
+        return *this;
+    }
+} Dec;
+struct sort_impl {
+    template <class T>
+    const sort_impl &operator>>(std::vector<T> &v) const {
+        std::sort(v.begin(), v.end());
+        return *this;
+    }
+} Sort;
+struct unique_impl {
+    template <class T>
+    const unique_impl &operator>>(std::vector<T> &v) const {
+        std::sort(v.begin(), v.end());
+        v.erase(std::unique(v.begin(), v.end()), v.end());
+        return *this;
+    }
+} Uniq;
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> p(n), q(n);
+    cin >> p >> q;
+    Dec >> p >> q;
+    vector<int> v(n), u(n);
+    rep (i, n) {
+        v[p[i]] = i;
+        u[q[i]] = i;
+    }
+    p = v, q = u;
+    vector<string> ans(n, string(n, '0'));
+    rep (i, n) {
+        rep (j, n) {
+            if (j >= n - i - 1)
+                ans[i][j] = '1';
+        }
+    }
+    vector<int> ord(n), inv(n);
+    iota(all(ord), 0);
+    iota(all(inv), 0);
+    rep (i, n) {
+        int j = inv[p[i]];
+        swap(ans[i], ans[j]);
+        swap(inv[ord[i]], inv[p[i]]);
+        swap(ord[i], ord[j]);
+    }
+    iota(all(ord), 0);
+    iota(all(inv), 0);
+    rep (i, n) {
+        int j = inv[q[i]];
+        rep (k, n) swap(ans[k][i], ans[k][j]);
+        swap(inv[ord[i]], inv[q[i]]);
+        swap(ord[i], ord[j]);
+    }
+    rep (i, n) co(ans[i]);
+    return 0;
+}

1d5486a9da77339d60dc479bb69402b1565801aa
GitHub <noreply@github.com>
2024-11-16 23:23:55 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc380/abc380_e.cpp
@@ -0,0 +1,186 @@
+// competitive-verifier: PROBLEM
+#include <utility>
+#include <vector>
+/**
+ * @brief ランレングス圧縮
+ *
+ * @tparam Container
+ * @param v 配列
+ * @return auto
+ */
+template <class Container>
+auto run_length_encoding(const Container &v) {
+    std::vector<std::pair<std::decay_t<decltype(v[0])>, int>> res;
+    for (auto &&e : v) {
+        if (res.empty() || res.back().first != e) res.emplace_back(e, 1);
+        else ++res.back().second;
+    }
+    return res;
+}
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = 3.14159265358979323846;
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+    constexpr void operator()() const {}
+} sonic;
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
+/**
+ * @brief 素集合データ構造
+ * @details Implement (union by size) + (path compression)
+ * @see https://github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
+ */
+struct union_find {
+    union_find() = default;
+    explicit union_find(int _n) : _rank(_n), data(_n, -1) {}
+    const int &operator[](std::size_t x) const { return data[x]; }
+    int &operator[](std::size_t x) { return data[x]; }
+    int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+    int get_root(int x) { return root(x); }
+    bool is_root(int x) const { return data[x] < 0; }
+    bool same(int x, int y) { return root(x) == root(y); }
+    bool is_same(int x, int y) { return same(x, y); }
+    int rank() { return _rank; }
+    int size(int x) { return -(data[root(x)]); }
+    int get_size(int x) { return size(x); }
+    std::vector<int> leaders() {
+        std::vector<int> res;
+        for (int i = 0; i < (int)data.size(); ++i) {
+            if (is_root(i)) res.emplace_back(i);
+        }
+        return res;
+    }
+    bool unite(int x, int y) {
+        x = root(x), y = root(y);
+        if (x == y) return false;
+        --_rank;
+        if (data[x] > data[y]) std::swap(x, y);
+        data[x] += data[y];
+        data[y] = x;
+        return true;
+    }
+    template <class F>
+    bool unite(int x, int y, F f) {
+        x = root(x), y = root(y);
+        if (x != y) {
+            if (data[x] > data[y]) std::swap(x, y);
+            data[x] += data[y];
+            data[y] = x;
+        }
+        f(x, y);
+        return x != y;
+    }
+  private:
+    int _rank;
+    std::vector<int> data;
+};
+int main(void) {
+    int n, q;
+    cin >> n >> q;
+    union_find uf(n);
+    vector<int> color(n);
+    vector<int> l(n), r(n);
+    rep (i, n) {
+        color[i] = i;
+        l[i] = r[i] = i;
+    }
+    vector<int> cnt(n, 1);
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int x, c;
+            cin >> x >> c;
+            --x, --c;
+            x = uf.root(x);
+            cnt[color[x]] -= r[x] - l[x] + 1;
+            cnt[c] += r[x] - l[x] + 1;
+            if (l[x] > 0 && color[uf.root(l[x] - 1)] == c) {
+                auto f = [&](int x, int y) {
+                    chmin(l[x], l[y]);
+                    chmax(r[x], r[y]);
+                };
+                uf.unite(l[x], l[x] - 1, f);
+            }
+            if (r[x] < n - 1 && color[uf.root(r[x] + 1)] == c) {
+                auto f = [&](int x, int y) {
+                    chmin(l[x], l[y]);
+                    chmax(r[x], r[y]);
+                };
+                uf.unite(r[x], r[x] + 1, f);
+            }
+            x = uf.root(x);
+            color[x] = c;
+        } else {
+            int c;
+            cin >> c;
+            co(cnt[c - 1]);
+        }
+    }
+    return 0;
+}

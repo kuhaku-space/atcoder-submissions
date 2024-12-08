310d90a9bbd2ae9ce70c080e05b50d95b7363e35
GitHub <noreply@github.com>
2024-12-08 23:23:01 +0000

    [auto-commit] Add my codes

---
+++ code/abc/abc383/abc383_e.cpp
@@ -0,0 +1,187 @@
+// competitive-verifier: PROBLEM
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = 3.14159265358979323846;
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+    constexpr void operator()() const {}
+} sonic;
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
+struct increment_impl {
+    template <class T>
+    const increment_impl &operator>>(std::vector<T> &v) const {
+        for (auto &x : v) ++x;
+        return *this;
+    }
+} Inc;
+struct decrement_impl {
+    template <class T>
+    const decrement_impl &operator>>(std::vector<T> &v) const {
+        for (auto &x : v) --x;
+        return *this;
+    }
+} Dec;
+struct sort_impl {
+    template <class T>
+    const sort_impl &operator>>(std::vector<T> &v) const {
+        std::sort(v.begin(), v.end());
+        return *this;
+    }
+} Sort;
+struct unique_impl {
+    template <class T>
+    const unique_impl &operator>>(std::vector<T> &v) const {
+        std::sort(v.begin(), v.end());
+        v.erase(std::unique(v.begin(), v.end()), v.end());
+        return *this;
+    }
+} Uniq;
+/**
+ * @brief 素集合データ構造
+ * @details Implement (union by size) + (path compression)
+ * @see https://github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
+ */
+struct union_find {
+    union_find() = default;
+    explicit union_find(int _n) : _rank(_n), data(_n, -1) {}
+    const int &operator[](std::size_t x) const { return data[x]; }
+    int &operator[](std::size_t x) { return data[x]; }
+    int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+    int get_root(int x) { return root(x); }
+    bool is_root(int x) const { return data[x] < 0; }
+    bool same(int x, int y) { return root(x) == root(y); }
+    bool is_same(int x, int y) { return same(x, y); }
+    int rank() { return _rank; }
+    int size(int x) { return -(data[root(x)]); }
+    int get_size(int x) { return size(x); }
+    std::vector<int> leaders() {
+        std::vector<int> res;
+        for (int i = 0; i < (int)data.size(); ++i) {
+            if (is_root(i)) res.emplace_back(i);
+        }
+        return res;
+    }
+    bool unite(int x, int y) {
+        x = root(x), y = root(y);
+        if (x == y) return false;
+        --_rank;
+        if (data[x] > data[y]) std::swap(x, y);
+        data[x] += data[y];
+        data[y] = x;
+        return true;
+    }
+    template <class F>
+    bool unite(int x, int y, F f) {
+        x = root(x), y = root(y);
+        if (x != y) {
+            if (data[x] > data[y]) std::swap(x, y);
+            data[x] += data[y];
+            data[y] = x;
+        }
+        f(x, y);
+        return x != y;
+    }
+  private:
+    int _rank;
+    std::vector<int> data;
+};
+int main(void) {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<tuple<int, int, int>> edge(m);
+    for (auto &[u, v, w] : edge) cin >> u >> v >> w;
+    sort(all(edge), [](auto p, auto q) {
+        return get<2>(p) < get<2>(q);
+    });
+    vector<int> a(k), b(k);
+    cin >> a >> b;
+    Dec >> a >> b;
+    union_find uf(n);
+    vector<int> ca(n), cb(n);
+    rep (i, k) {
+        ++ca[a[i]];
+        ++cb[b[i]];
+    }
+    ll ans = 0;
+    for (auto [u, v, w] : edge) {
+        --u, --v;
+        auto f = [&](int x, int y) {
+            if (x == y)
+                return;
+            ca[x] += ca[y];
+            cb[x] += cb[y];
+            int c = min(ca[x], cb[x]);
+            ans += (ll)w * c;
+            ca[x] -= c;
+            cb[x] -= c;
+        };
+        uf.unite(u, v, f);
+    }
+    co(ans);
+    return 0;
+}
---
+++ code/arc/arc189/arc189_d.cpp
@@ -0,0 +1,231 @@
+// competitive-verifier: PROBLEM
+#include <algorithm>
+#include <iterator>
+#include <vector>
+/**
+ * @brief 座標圧縮
+ *
+ * @tparam T 要素の型
+ */
+template <class T>
+struct coordinate_compression {
+    coordinate_compression() = default;
+    coordinate_compression(const std::vector<T> &_data) : data(_data) { build(); }
+    const T &operator[](int i) const { return data[i]; }
+    void add(T x) { data.emplace_back(x); }
+    void build() {
+        std::sort(data.begin(), data.end());
+        data.erase(std::unique(data.begin(), data.end()), data.end());
+    }
+    bool exists(T x) const {
+        auto it = std::lower_bound(data.begin(), data.end(), x);
+        return it != data.end() && *it == x;
+    }
+    int get(T x) const {
+        auto it = std::lower_bound(data.begin(), data.end(), x);
+        return std::distance(data.begin(), it);
+    }
+    int size() const { return data.size(); }
+  private:
+    std::vector<T> data;
+};
+/**
+ * @brief 座標圧縮
+ *
+ * @tparam T 要素の型
+ * @param v 配列
+ * @return std::vector<T>
+ */
+template <class T>
+std::vector<int> compress(const std::vector<T> &v) {
+    coordinate_compression cps(v);
+    std::vector<int> res;
+    res.reserve(std::size(v));
+    for (auto &&x : v) res.emplace_back(cps.get(x));
+    return res;
+}
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = 3.14159265358979323846;
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+    constexpr void operator()() const {}
+} sonic;
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
+/**
+ * @brief 素集合データ構造
+ * @details Implement (union by size) + (path compression)
+ * @see https://github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
+ */
+struct union_find {
+    union_find() = default;
+    explicit union_find(int _n) : _rank(_n), data(_n, -1) {}
+    const int &operator[](std::size_t x) const { return data[x]; }
+    int &operator[](std::size_t x) { return data[x]; }
+    int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+    int get_root(int x) { return root(x); }
+    bool is_root(int x) const { return data[x] < 0; }
+    bool same(int x, int y) { return root(x) == root(y); }
+    bool is_same(int x, int y) { return same(x, y); }
+    int rank() { return _rank; }
+    int size(int x) { return -(data[root(x)]); }
+    int get_size(int x) { return size(x); }
+    std::vector<int> leaders() {
+        std::vector<int> res;
+        for (int i = 0; i < (int)data.size(); ++i) {
+            if (is_root(i)) res.emplace_back(i);
+        }
+        return res;
+    }
+    bool unite(int x, int y) {
+        x = root(x), y = root(y);
+        if (x == y) return false;
+        --_rank;
+        if (data[x] > data[y]) std::swap(x, y);
+        data[x] += data[y];
+        data[y] = x;
+        return true;
+    }
+    template <class F>
+    bool unite(int x, int y, F f) {
+        x = root(x), y = root(y);
+        if (x != y) {
+            if (data[x] > data[y]) std::swap(x, y);
+            data[x] += data[y];
+            data[y] = x;
+        }
+        f(x, y);
+        return x != y;
+    }
+  private:
+    int _rank;
+    std::vector<int> data;
+};
+int main(void) {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    cin >> a;
+    coordinate_compression cps(a);
+    auto b = compress(a);
+    vector<vector<int>> v(n);
+    rep (i, n) v[b[i]].emplace_back(i);
+    vector<ll> ans(n, -1);
+    union_find uf(n);
+    vector<vector<int>> indexes(n);
+    vector<ll> weight(n);
+    rep (i, n) indexes[i].emplace_back(i);
+    rep (i, n) weight[i] = a[i];
+    rep (i, n) {
+        auto u = v[i];
+        if (u.empty())
+            continue;
+        for (int x : u) {
+            bool f = false;
+            f |= x > 0 && a[x - 1] < a[x];
+            f |= x < n - 1 && a[x] > a[x + 1];
+            if (!f) {
+                ans[x] = a[x];
+                indexes[x].clear();
+            }
+        }
+        for (int x : u) {
+            if (0 < x && a[x - 1] < a[x] && weight[uf.root(x - 1)] <= a[x]) {
+                for (int y : indexes[uf.root(x - 1)]) {
+                    ans[y] = weight[uf.root(x - 1)];
+                }
+                indexes[uf.root(x - 1)].clear();
+            }
+            if (x < n - 1 && a[x] > a[x + 1] && a[x] >= weight[uf.root(x + 1)]) {
+                for (int y : indexes[uf.root(x + 1)]) {
+                    ans[y] = weight[uf.root(x + 1)];
+                }
+                indexes[uf.root(x + 1)].clear();
+            }
+        }
+        auto f = [&](int x, int y) {
+            if (x == y)
+                return;
+            weight[x] += weight[y];
+            indexes[x].insert(indexes[x].end(), indexes[y].begin(), indexes[y].end());
+            indexes[y].clear();
+        };
+        for (int x : u) {
+            if (0 < x && a[x - 1] <= a[x])
+                uf.unite(x - 1, x, f);
+            if (x < n - 1 && a[x] >= a[x + 1])
+                uf.unite(x, x + 1, f);
+        }
+    }
+    rep (i, n) {
+        if (ans[i] == -1)
+            ans[i] = weight[uf.root(i)];
+    }
+    co(ans);
+    return 0;
+}

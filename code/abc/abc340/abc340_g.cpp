327e7cb678cf8afc7d2edc1ea088125c515a87b0
GitHub <noreply@github.com>
2025-03-12 23:24:01 +0000

    [auto-commit] Add my codes

--- code/abc/abc340/abc340_g.cpp
+++ code/abc/abc340/abc340_g.cpp
@@ -895,25 +895,18 @@ struct linear_lca {
     std::vector<int> ord;
     linear_sparse_table<M> lst;
 };
-struct auxiliary_tree {
+struct auxiliary_tree : public Graph<void> {
     auxiliary_tree(const std::vector<int> &_ord, const std::vector<int> &_par,
                    const std::vector<bool> &_f)
-        : g(_par.size()), ord(_ord), f(_f) {
+        : Graph<void>::Graph(_par.size()), ord(_ord), f(_f) {
         int n = _par.size();
         for (int i = 0; i < n; ++i) {
-            if (_par[i] != -1) g.add_edges(_par[i], i);
+            if (_par[i] != -1) add_edges(_par[i], i);
         }
     }
-    const auto &operator[](int i) const { return g[i]; }
-    auto &operator[](int i) { return g[i]; }
-    const auto begin() const { return g.begin(); }
-    auto begin() { return g.begin(); }
-    const auto end() const { return g.end(); }
-    auto end() { return g.end(); }
     int vertex(int x) const { return ord[x]; }
     bool contains(int x) const { return f[x]; }
   private:
-    Graph<void> g;
     std::vector<int> ord;
     std::vector<bool> f;
 };
@@ -950,6 +943,50 @@ struct auxiliary_tree_builder {
     linear_lca lca;
     euler_tour et;
 };
+/**
+ * @brief 重心分解
+ *
+ * @tparam T 辺の重みの型
+ * @param g グラフ
+ * @return std::vector<int> 親の頂点
+ */
+template <class T>
+std::vector<int> centroid_decomposition(const Graph<T> &g) {
+    int n = g.size();
+    std::vector<int> par(n, -1), size(n), size_par(n, -2);
+    std::vector<bool> used(n, false);
+    auto dfs = [&](auto self, int x, int p) -> int {
+        if (size_par[x] == p) return size[x];
+        int sum = 1;
+        for (auto e : g[x]) {
+            if (used[e.to()] || e.to() == p) continue;
+            sum += self(self, e.to(), x);
+        }
+        size_par[x] = p;
+        return size[x] = sum;
+    };
+    auto build = [&](auto self, int x, int p) -> void {
+        int sz = dfs(dfs, x, p);
+        bool is_centroid = false;
+        while (!is_centroid) {
+            is_centroid = true;
+            for (auto e : g[x]) {
+                if (size[e.to()] > size[x] || size[e.to()] * 2 <= sz) continue;
+                x = e.to();
+                is_centroid = false;
+                break;
+            }
+        }
+        par[x] = p;
+        used[x] = true;
+        for (auto e : g[x]) {
+            if (used[e.to()]) continue;
+            self(self, e.to(), x);
+        }
+    };
+    build(build, 0, -1);
+    return par;
+}
 using Mint = modint998;
 int main(void) {
     int n;

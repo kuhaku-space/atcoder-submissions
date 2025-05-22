b94b543dde5aa18f4ee53df06d72c30babc58c5d
GitHub <noreply@github.com>
2025-05-22 23:25:04 +0000

    [auto-commit] Add my codes

--- code/other/practice2/practice2_e.cpp
+++ code/other/practice2/practice2_e.cpp
@@ -1,94 +1,107 @@
-#include <bits/stdc++.h>
+// competitive-verifier: PROBLEM https://atcoder.jp/contests/practice2/tasks/practice2_e
 #include <atcoder/all>
-using namespace std;
-using namespace atcoder;
-using ll = long long;
-using ld = long double;
-using P = pair<ll, ll>;
-using Pld = pair<ld, ld>;
-using Vec = vector<ll>;
-using VecP = vector<P>;
-using VecB = vector<bool>;
-using VecC = vector<char>;
-using VecD = vector<ld>;
-using VecS = vector<string>;
-template <class T>
-using Vec2 = vector<vector<T>>;
-#define REP(i, m, n) for(ll i = (m); i < (n); ++i)
-#define REPN(i, m, n) for(ll i = (m); i <= (n); ++i)
-#define REPR(i, m, n) for(ll i = (m)-1; i >= (n); --i)
-#define REPNR(i, m, n) for(ll i = (m); i >= (n); --i)
-#define rep(i, n) REP(i, 0, n)
-#define repn(i, n) REPN(i, 1, n)
-#define repr(i, n) REPR(i, n, 0)
-#define repnr(i, n) REPNR(i, n, 1)
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = 3.14159265358979323846;
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
 #define all(s) (s).begin(), (s).end()
-#define pb push_back
-#define fs first
-#define sc second
-template <class T1, class T2>
-bool chmax(T1 &a, const T2 b){if(a < b){a = b; return true;} return false;}
-template <class T1, class T2>
-bool chmin(T1 &a, const T2 b){if(a > b){a = b; return true;} return false;}
-ll pow2(const int n){return (1LL << n);}
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+    constexpr void operator()() const {}
+} sonic;
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
 template <class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-    for (const T &i : v) os << i << ' ';
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
     return os;
 }
-void co() { cout << '\n'; }
 template <class Head, class... Tail>
-void co(Head&& head, Tail&&... tail) {
-    cout << head << ' ';
-    co(forward<Tail>(tail)...);
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
 }
-void ce() { cerr << '\n'; }
 template <class Head, class... Tail>
-void ce(Head&& head, Tail&&... tail) {
-    cerr << head << ' ';
-    ce(forward<Tail>(tail)...);
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
 }
-void sonic(){ios::sync_with_stdio(false); cin.tie(0);}
-void setp(const int n){cout << fixed << setprecision(n);}
-constexpr int INF = 1000000001;
-constexpr ll LINF = 1000000000000000001;
-constexpr ll MOD = 1000000007;
-constexpr ll MOD_N = 998244353;
-constexpr ld EPS = 1e-11;
-const double PI = acos(-1);
-
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
 int main(void) {
     ll n, k;
     cin >> n >> k;
-    Vec2<ll> a(n, Vec(n));
-    rep(i, n) rep(j, n) cin >> a[i][j];
-
-    const ll sz = n * n + 2 * n + 2, n2 = n * n;
-    mcf_graph<int, ll> mcf(sz);
+    vector a(n, vector<ll>(n));
+    cin >> a;
+    ll sz = n * n + 2 * n + 2, n2 = n * n;
+    atcoder::mcf_graph<int, ll> mcf(sz);
     mcf.add_edge(sz - 2, sz - 1, n * k, INF);
-    rep(i, n){
+    rep (i, n) {
         mcf.add_edge(sz - 2, n2 + i, k, 0);
         mcf.add_edge(n2 + n + i, sz - 1, k, 0);
-        rep(j, n){
+        rep (j, n) {
             mcf.add_edge(n2 + i, i * n + j, 1, INF - a[i][j]);
             mcf.add_edge(i * n + j, n2 + n + j, 1, 0);
         }
     }
-
     auto p = mcf.flow(sz - 2, sz - 1, n * k);
     co(n * k * INF - p.second);
-
     vector<string> ans;
     string s;
-    rep(i, n) s += '.';
-    rep(i, n) ans.push_back(s);
+    rep (i, n) s += '.';
+    rep (i, n) ans.push_back(s);
     auto edges = mcf.edges();
     for (auto e : edges) {
-        if(e.to >= 0 && e.to < n2){
-            if (e.flow) ans[e.to / n][e.to % n] = 'X';
+        if (e.to >= 0 && e.to < n2) {
+            if (e.flow)
+                ans[e.to / n][e.to % n] = 'X';
         }
     }
-    rep(i, n) co(ans[i]);
-
+    rep (i, n) co(ans[i]);
     return 0;
 }

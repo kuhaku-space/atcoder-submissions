aab475ca013c4c2aa144017f70ab008b8fb7a947
GitHub <noreply@github.com>
2025-05-23 23:24:34 +0000

    [auto-commit] Add my codes

--- code/abc/abc332/abc332_d.cpp
+++ code/abc/abc332/abc332_d.cpp
@@ -1,66 +1,53 @@
-#line 1 "a.cpp"
-#define PROBLEM ""
-#line 1 "/home/kuhaku/home/github/algo/lib/algorithm/inversion_number.hpp"
+// competitive-verifier: PROBLEM https://yukicoder.me/problems/no/1209
 #include <algorithm>
-#include <iterator>
-#line 4 "/home/kuhaku/home/github/algo/lib/algorithm/compress.hpp"
+#include <cstdint>
+#include <numeric>
 #include <vector>
-
-/**
- * @brief 座標圧縮
- *
- * @tparam T 要素の型
- */
+#include <iterator>
+/// @brief 座標圧縮
 template <class T>
 struct coordinate_compression {
     coordinate_compression() = default;
     coordinate_compression(const std::vector<T> &_data) : data(_data) { build(); }
-
     const T &operator[](int i) const { return data[i]; }
-    T &operator[](int i) { return data[i]; }
-
     void add(T x) { data.emplace_back(x); }
-
     void build() {
-        std::sort(std::begin(data), std::end(data));
-        data.erase(std::unique(std::begin(data), std::end(data)), std::end(data));
+        std::sort(data.begin(), data.end());
+        data.erase(std::unique(data.begin(), data.end()), data.end());
     }
-
     bool exists(T x) const {
-        auto it = std::lower_bound(std::begin(data), std::end(data), x);
-        return it != std::end(data) && *it == x;
+        auto it = std::lower_bound(data.begin(), data.end(), x);
+        return it != data.end() && *it == x;
     }
-
     int get(T x) const {
-        auto it = std::lower_bound(std::begin(data), std::end(data), x);
-        return std::distance(std::begin(data), it);
+        return std::distance(data.begin(), std::lower_bound(data.begin(), data.end(), x));
     }
-
-    int size() const { return std::size(data); }
-
+    int lower_bound(T x) const {
+        return std::distance(data.begin(), std::lower_bound(data.begin(), data.end(), x));
+    }
+    int upper_bound(T x) const {
+        return std::distance(data.begin(), std::upper_bound(data.begin(), data.end(), x));
+    }
+    std::vector<int> compress(const std::vector<T> &v) const {
+        int n = v.size();
+        std::vector<int> res(n);
+        for (int i = 0; i < n; ++i) res[i] = get(v[i]);
+        return res;
+    }
+    int size() const { return data.size(); }
   private:
     std::vector<T> data;
 };
-
-/**
- * @brief 座標圧縮
- *
- * @tparam T 要素の型
- * @param v 配列
- * @return std::vector<T>
- */
+/// @brief 座標圧縮
 template <class T>
-std::vector<T> compress(const std::vector<T> &v) {
+std::vector<int> compress(const std::vector<T> &v) {
     coordinate_compression cps(v);
-    std::vector<T> res;
+    std::vector<int> res;
     res.reserve(std::size(v));
     for (auto &&x : v) res.emplace_back(cps.get(x));
     return res;
 }
-#line 2 "/home/kuhaku/home/github/algo/lib/binary_tree/fenwick_tree.hpp"
 #include <cassert>
-#line 4 "/home/kuhaku/home/github/algo/lib/binary_tree/fenwick_tree.hpp"
-
 /**
  * @brief フェニック木
  * @see http://hos.ac/slides/20140319_bit.pdf
@@ -75,93 +62,56 @@ struct fenwick_tree {
     fenwick_tree(const std::vector<U> &v) : _size((int)v.size() + 1), data((int)v.size() + 1) {
         build(v);
     }
-
-    T operator[](int i) const { return sum(i + 1) - sum(i); }
+    T operator[](int i) const { return sum(i, i + 1); }
     T at(int k) const { return operator[](k); }
     T get(int k) const { return operator[](k); }
-
     template <class U>
     void build(const std::vector<U> &v) {
-        for (int i = 0, n = v.size(); i < n; ++i) add(i, v[i]);
+        for (int i = 0, n = v.size(); i < n; ++i) data[i + 1] = v[i];
+        for (int i = 1; i < _size; ++i) {
+            if (i + (i & -i) < _size) data[i + (i & -i)] += data[i];
+        }
     }
-
-    /**
-     * @brief v[k] = val
-     *
-     * @param k index of array
-     * @param val new value
-     * @return void
-     */
-    void update(int k, T val) { add(k, val - at(k)); }
-    /**
-     * @brief v[k] += val
-     *
-     * @param k index of array
-     * @param val new value
-     * @return void
-     */
+    void set(int k, T val) { add(k, val - at(k)); }
+    void update(int k, T val) { set(k); }
     void add(int k, T val) {
-        assert(0 <= k && k < _size);
+        assert(0 <= k && k < _size - 1);
         for (++k; k < _size; k += k & -k) data[k] += val;
     }
-    /**
-     * @brief chmax(v[k], val)
-     *
-     * @param k index of array
-     * @param val new value
-     * @return bool
-     */
     bool chmax(int k, T val) {
         if (at(k) >= val) return false;
-        update(k, val);
+        set(k, val);
         return true;
     }
-    /**
-     * @brief chmin(v[k], val)
-     *
-     * @param k index of value
-     * @param val new value
-     * @return bool
-     */
     bool chmin(int k, T val) {
         if (at(k) <= val) return false;
-        update(k, val);
+        set(k, val);
         return true;
     }
-
-    /**
-     * @brief v[0] + ... + v[n - 1]
-     *
-     * @return T
-     */
-    T all_sum() const { return sum(_size); }
-    /**
-     * @brief v[0] + ... + v[k - 1]
-     *
-     * @param k index of array
-     * @return T
-     */
+    T all_prod() const { return prod(_size - 1); }
+    T prod(int k) const { return sum(k); }
+    T prod(int a, int b) const { return sum(a, b); }
+    T all_sum() const { return sum(_size - 1); }
     T sum(int k) const {
-        assert(0 <= k && k <= _size);
+        assert(0 <= k && k < _size);
         T res = 0;
         for (; k > 0; k -= k & -k) res += data[k];
         return res;
     }
-    /**
-     * @brief v[a] + ... + v[b - 1]
-     *
-     * @param a first index of array
-     * @param b last index of array
-     * @return T
-     */
-    T sum(int a, int b) const { return a < b ? sum(b) - sum(a) : 0; }
-
-    /**
-     * @brief binary search on fenwick_tree
-     *
-     * @param val target value
-     * @return int
-     */
+    T sum(int a, int b) const {
+        assert(0 <= a && a <= b && b < _size);
+        T res = T();
+        while (a != b) {
+            if (a < b) {
+                res += data[b];
+                b -= b & -b;
+            } else {
+                res -= data[a];
+                a -= a & -a;
+            }
+        }
+        return res;
+    }
     int lower_bound(T val) const {
         if (val <= 0) return 0;
         int k = 1;
@@ -172,25 +122,17 @@ struct fenwick_tree {
         }
         return res;
     }
-
   private:
     int _size;
     std::vector<T> data;
 };
-#line 5 "/home/kuhaku/home/github/algo/lib/algorithm/inversion_number.hpp"
-
-/**
- * @brief 転倒数を求める
- *
- * @tparam T 配列の型
- * @param v 配列
- * @retval std::int64_t 転倒数
- */
+/// @brief 転倒数
 template <class T>
 std::int64_t inversion_number(const std::vector<T> &v) {
+    if (v.empty()) return 0;
     auto u = compress(v);
-    std::reverse(std::begin(u), std::end(u));
-    fenwick_tree<T> bit(*max_element(std::begin(u), std::end(u)) + 1);
+    std::reverse(u.begin(), u.end());
+    fenwick_tree<T> bit(*std::max_element(u.begin(), u.end()) + 1);
     std::int64_t res = 0;
     for (auto x : u) {
         res += bit.sum(x);
@@ -198,11 +140,45 @@ std::int64_t inversion_number(const std::vector<T> &v) {
     }
     return res;
 }
-#line 2 "/home/kuhaku/home/github/algo/lib/template/template.hpp"
-#pragma GCC target("sse4.2,avx2,bmi2")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
+/// @brief 転倒数
+template <class T>
+std::int64_t inversion_number_of_permutation(const std::vector<T> &v) {
+    if (v.empty()) return 0;
+    int n = v.size();
+    fenwick_tree<T> bit(n);
+    std::int64_t res = 0;
+    for (int i = n - 1; i >= 0; --i) {
+        res += bit.sum(v[i]);
+        bit.add(v[i], 1);
+    }
+    return res;
+}
+/// @brief 最小隣接スワップ回数
+template <class T>
+std::int64_t swap_distance(const std::vector<T> &a, const std::vector<T> &b) {
+    if (a.size() != b.size()) return -1;
+    int n = a.size();
+    std::vector<int> c(n), d(n);
+    std::iota(c.begin(), c.end(), 0);
+    std::iota(d.begin(), d.end(), 0);
+    std::stable_sort(c.begin(), c.end(), [&a](int x, int y) { return a[x] < a[y]; });
+    std::stable_sort(d.begin(), d.end(), [&b](int x, int y) { return b[x] < b[y]; });
+    std::vector<int> p(n);
+    for (int i = 0; i < n; ++i) {
+        int x = c[i], y = d[i];
+        if (a[x] != b[y]) return -1;
+        p[x] = y;
+    }
+    return inversion_number_of_permutation(p);
+}
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
 #include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
 template <class T, class U>
 constexpr bool chmax(T &a, const U &b) {
     return a < (T)b ? a = (T)b, true : false;
@@ -214,8 +190,7 @@ constexpr bool chmin(T &a, const U &b) {
 constexpr std::int64_t INF = 1000000000000000003;
 constexpr int Inf = 1000000003;
 constexpr double EPS = 1e-7;
-constexpr double PI = M_PI;
-#line 3 "/home/kuhaku/home/github/algo/lib/template/macro.hpp"
+constexpr double PI = 3.14159265358979323846;
 #define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
 #define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
 #define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
@@ -224,17 +199,14 @@ constexpr double PI = M_PI;
 #define repr(i, n) FORR (i, n, 0)
 #define repnr(i, n) FORR (i, n + 1, 1)
 #define all(s) (s).begin(), (s).end()
-#line 3 "/home/kuhaku/home/github/algo/lib/template/sonic.hpp"
 struct Sonic {
     Sonic() {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout << std::fixed << std::setprecision(20);
     }
-
     constexpr void operator()() const {}
 } sonic;
-#line 5 "/home/kuhaku/home/github/algo/lib/template/atcoder.hpp"
 using namespace std;
 using ll = std::int64_t;
 using ld = long double;
@@ -253,9 +225,7 @@ std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
 }
 template <class T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
-    for (auto it = v.begin(); it != v.end(); ++it) {
-        os << (it == v.begin() ? "" : " ") << *it;
-    }
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
     return os;
 }
 template <class Head, class... Tail>
@@ -268,88 +238,23 @@ void ce(Head &&head, Tail &&...tail) {
     if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
     else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
 }
-template <typename T, typename... Args>
-auto make_vector(T x, int arg, Args... args) {
-    if constexpr (sizeof...(args) == 0) return std::vector<T>(arg, x);
-    else return std::vector(arg, make_vector<T>(x, args...));
-}
-void Yes(bool is_correct = true) {
-    std::cout << (is_correct ? "Yes" : "No") << '\n';
-}
-void No(bool is_not_correct = true) {
-    Yes(!is_not_correct);
-}
-void YES(bool is_correct = true) {
-    std::cout << (is_correct ? "YES" : "NO") << '\n';
-}
-void NO(bool is_not_correct = true) {
-    YES(!is_not_correct);
-}
-void Takahashi(bool is_correct = true) {
-    std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n';
-}
-void Aoki(bool is_not_correct = true) {
-    Takahashi(!is_not_correct);
-}
-#line 3 "/home/kuhaku/home/github/algo/lib/tree/union_find.hpp"
-
-/**
- * @brief 素集合データ構造
- * @details Implement (union by size) + (path compression)
- * @see https://github.com/atcoder/ac-library/blob/master/atcoder/dsu.hpp
- */
-struct union_find {
-    union_find() = default;
-    explicit union_find(int _n) : data(_n, -1) {}
-
-    int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
-    int get_root(int x) { return root(x); }
-
-    bool is_root(int x) const { return data[x] < 0; }
-
-    bool same(int x, int y) { return root(x) == root(y); }
-    bool is_same(int x, int y) { return same(x, y); }
-
-    int size(int x) { return -(data[root(x)]); }
-    int get_size(int x) { return size(x); }
-
-    bool unite(int x, int y) {
-        x = root(x), y = root(y);
-        if (x == y) return false;
-        if (data[x] > data[y]) std::swap(x, y);
-        data[x] += data[y];
-        data[y] = x;
-        return true;
-    }
-
-    template <class F>
-    bool unite(int x, int y, F f) {
-        x = root(x), y = root(y);
-        if (x != y) {
-            if (data[x] > data[y]) std::swap(x, y);
-            data[x] += data[y];
-            data[y] = x;
-        }
-        f(x, y);
-        return x != y;
-    }
-
-  private:
-    std::vector<int> data;
-};
-#line 5 "a.cpp"
-
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
 int main(void) {
     int h, w;
     cin >> h >> w;
     vector a(h, vector(w, 0));
     auto b = a;
     cin >> a >> b;
-
     int ans = Inf;
     vector<int> ordx(h);
     iota(all(ordx), 0);
     do {
+        int x = inversion_number_of_permutation(ordx);
         vector<int> ordy(w);
         iota(all(ordy), 0);
         do {
@@ -360,10 +265,9 @@ int main(void) {
                 }
             }
             if (f)
-                chmin(ans, inversion_number(ordx) + inversion_number(ordy));
+                chmin(ans, x + inversion_number_of_permutation(ordy));
         } while (next_permutation(all(ordy)));
     } while (next_permutation(all(ordx)));
     co(ans == Inf ? -1 : ans);
-
     return 0;
 }
--- code/arc/arc120/arc120_c.cpp
+++ code/arc/arc120/arc120_c.cpp
@@ -1,187 +1,258 @@
-#line 1 "a.cpp"
-#define PROBLEM ""
-#line 2 "/home/kuhaku/kuhaku/github/atcoder-lib/lib/template/template.hpp"
-#include <bits/stdc++.h>
-using namespace std;
-template <class T, class U>
-bool chmax(T &a, const U &b) {
-    return a < b ? a = b, true : false;
+// competitive-verifier: PROBLEM https://yukicoder.me/problems/no/1209
+#include <algorithm>
+#include <cstdint>
+#include <numeric>
+#include <vector>
+#include <iterator>
+/// @brief 座標圧縮
+template <class T>
+struct coordinate_compression {
+    coordinate_compression() = default;
+    coordinate_compression(const std::vector<T> &_data) : data(_data) { build(); }
+    const T &operator[](int i) const { return data[i]; }
+    void add(T x) { data.emplace_back(x); }
+    void build() {
+        std::sort(data.begin(), data.end());
+        data.erase(std::unique(data.begin(), data.end()), data.end());
+    }
+    bool exists(T x) const {
+        auto it = std::lower_bound(data.begin(), data.end(), x);
+        return it != data.end() && *it == x;
+    }
+    int get(T x) const {
+        return std::distance(data.begin(), std::lower_bound(data.begin(), data.end(), x));
+    }
+    int lower_bound(T x) const {
+        return std::distance(data.begin(), std::lower_bound(data.begin(), data.end(), x));
+    }
+    int upper_bound(T x) const {
+        return std::distance(data.begin(), std::upper_bound(data.begin(), data.end(), x));
+    }
+    std::vector<int> compress(const std::vector<T> &v) const {
+        int n = v.size();
+        std::vector<int> res(n);
+        for (int i = 0; i < n; ++i) res[i] = get(v[i]);
+        return res;
+    }
+    int size() const { return data.size(); }
+  private:
+    std::vector<T> data;
+};
+/// @brief 座標圧縮
+template <class T>
+std::vector<int> compress(const std::vector<T> &v) {
+    coordinate_compression cps(v);
+    std::vector<int> res;
+    res.reserve(std::size(v));
+    for (auto &&x : v) res.emplace_back(cps.get(x));
+    return res;
 }
-template <class T, class U>
-bool chmin(T &a, const U &b) {
-    return b < a ? a = b, true : false;
-}
-constexpr int64_t INF = 1000000000000000003;
-constexpr int Inf = 1000000003;
-constexpr int MOD = 1000000007;
-constexpr int MOD_N = 998244353;
-constexpr double EPS = 1e-7;
-const double PI = acos(-1.0);
-#line 2 "/home/kuhaku/kuhaku/github/atcoder-lib/lib/binary_tree/BIT.hpp"
-
+#include <cassert>
 /**
- * @brief Binary Indexed Tree
+ * @brief フェニック木
+ * @see http://hos.ac/slides/20140319_bit.pdf
+ *
+ * @tparam T
  */
 template <class T>
-struct BIT {
-    int N;
-    vector<T> data;
-
-    BIT() : N(), data() {}
-    BIT(int n, T e = T(0)) : N(n + 1), data(n + 1, e) {}
-    BIT(const vector<T> &v) : N(v.size() + 1), data(v.size() + 1) { this->build(v); }
-
-    const T &operator[](int i) const { return this->at(i); }
-    const T at(int k) const { return this->sum(k + 1) - this->sum(k); }
-    const T get(int k) const { return this->sum(k + 1) - this->sum(k); }
-
+struct fenwick_tree {
+    fenwick_tree() : _size(), data() {}
+    fenwick_tree(int n) : _size(n + 1), data(n + 1) {}
     template <class U>
-    void build(const vector<U> &v) {
-        int n = v.size();
-        for (int i = 0; i < n; ++i) this->add(i, v[i]);
+    fenwick_tree(const std::vector<U> &v) : _size((int)v.size() + 1), data((int)v.size() + 1) {
+        build(v);
     }
-
-    /**
-     * @brief v[k] = x
-     *
-     * @param k index
-     * @param x data
-     * @return void
-     */
-    void update(int k, T x) { this->add(k, x - this->at(k)); }
-    /**
-     * @brief v[k] += x
-     *
-     * @param k index
-     * @param x data
-     * @return void
-     */
-    void add(int k, T x) {
-        assert(0 <= k && k < N);
-        for (++k; k < N; k += k & -k) data[k] += x;
+    T operator[](int i) const { return sum(i, i + 1); }
+    T at(int k) const { return operator[](k); }
+    T get(int k) const { return operator[](k); }
+    template <class U>
+    void build(const std::vector<U> &v) {
+        for (int i = 0, n = v.size(); i < n; ++i) data[i + 1] = v[i];
+        for (int i = 1; i < _size; ++i) {
+            if (i + (i & -i) < _size) data[i + (i & -i)] += data[i];
+        }
     }
-    /**
-     * @brief chmax(v[k], x)
-     *
-     * @param k index
-     * @param x data
-     * @return bool
-     */
-    bool chmax(int k, T x) {
-        if (this->at(k) >= x) return false;
-        this->update(k, x);
+    void set(int k, T val) { add(k, val - at(k)); }
+    void update(int k, T val) { set(k); }
+    void add(int k, T val) {
+        assert(0 <= k && k < _size - 1);
+        for (++k; k < _size; k += k & -k) data[k] += val;
+    }
+    bool chmax(int k, T val) {
+        if (at(k) >= val) return false;
+        set(k, val);
         return true;
     }
-    /**
-     * @brief chmin(v[k], x)
-     *
-     * @param k index
-     * @param x data
-     * @return bool
-     */
-    bool chmin(int k, T x) {
-        if (this->at(k) <= x) return false;
-        this->update(k, x);
+    bool chmin(int k, T val) {
+        if (at(k) <= val) return false;
+        set(k, val);
         return true;
     }
-
-    /**
-     * @brief v[0] + ... + v[k - 1]
-     *
-     * @param k index
-     * @return T
-     */
+    T all_prod() const { return prod(_size - 1); }
+    T prod(int k) const { return sum(k); }
+    T prod(int a, int b) const { return sum(a, b); }
+    T all_sum() const { return sum(_size - 1); }
     T sum(int k) const {
-        assert(0 <= k && k <= N);
+        assert(0 <= k && k < _size);
         T res = 0;
         for (; k > 0; k -= k & -k) res += data[k];
         return res;
     }
-    /**
-     * @brief v[a] + ... + v[a - 1]
-     *
-     * @param a first index
-     * @param b last index
-     * @return T
-     */
-    T sum(int a, int b) const { return this->sum(b) - this->sum(a); }
-
-    /**
-     * @brief lower_bound(v.begin(), v.end(), x)
-     *
-     * @param x data
-     * @return int
-     */
-    int lower_bound(T x) const {
-        if (x <= 0) return 0;
-        int k = 0;
-        while (k < N) k <<= 1;
-        int res = 0;
-        for (; k > 0; k >>= 1) {
-            if (res + k < N && data[res + k] < x) x -= data[res += k];
+    T sum(int a, int b) const {
+        assert(0 <= a && a <= b && b < _size);
+        T res = T();
+        while (a != b) {
+            if (a < b) {
+                res += data[b];
+                b -= b & -b;
+            } else {
+                res -= data[a];
+                a -= a & -a;
+            }
         }
         return res;
     }
-};
-#line 3 "/home/kuhaku/kuhaku/github/atcoder-lib/lib/template/atcoder.hpp"
-using ll = int64_t;
-using ld = long double;
-#define FOR(i, m, n) for(int i = (m); i < (n); ++i)
-#define FORR(i, m, n) for(int i = (m)-1; i >= (n); --i)
-#define rep(i, n) FOR(i, 0, n)
-#define repn(i, n) FOR(i, 1, n+1)
-#define repr(i, n) FORR(i, n, 0)
-#define repnr(i, n) FORR(i, n+1, 1)
-#define all(s) (s).begin(), (s).end()
-template <class T>
-istream &operator>>(istream &is, vector<T> &v) { for (T &i : v) is>>i; return is; }
-template <class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-    for (auto it=v.begin(); it!=v.end(); ++it) { os<<(it==v.begin()?"":" ")<<*it; } return os;
-}
-template <class Head, class... Tail>
-void co(Head&& head, Tail&&... tail) {
-    if constexpr(sizeof...(tail)==0) cout<<head<<'\n'; else cout<<head<<' ',co(forward<Tail>(tail)...);
-}
-template <class Head, class... Tail>
-void ce(Head&& head, Tail&&... tail) {
-    if constexpr(sizeof...(tail)==0) cerr<<head<<'\n'; else cerr<<head<<' ',ce(forward<Tail>(tail)...);
-}
-template<typename T, typename... Args>
-auto make_vector(T x, int arg, Args ...args) {
-    if constexpr(sizeof...(args)==0) return vector<T>(arg, x); else return vector(arg,make_vector<T>(x, args...));
-}
-void sonic() { ios::sync_with_stdio(false); cin.tie(nullptr); }
-void setp(const int n) { cout << fixed << setprecision(n); }
-#line 4 "a.cpp"
-
-int main(void) {
-    int n;
-    cin >> n;
-    vector<int> a(n), b(n);
-    cin >> a >> b;
-
-    unordered_map<int, deque<int>> mp;
-    rep(i, n) {
-        mp[b[i] + i].emplace_back(i);
-    }
-    vector<int> v(n);
-    rep(i, n) {
-        if (mp[a[i] + i].empty()) {
-            co(-1);
-            return 0;
+    int lower_bound(T val) const {
+        if (val <= 0) return 0;
+        int k = 1;
+        while (k < _size) k <<= 1;
+        int res = 0;
+        for (; k > 0; k >>= 1) {
+            if (res + k < _size && data[res + k] < val) val -= data[res += k];
         }
-        v[i] = mp[a[i] + i].front();
-        mp[a[i] + i].pop_front();
+        return res;
     }
-
-    ll ans = 0;
-    BIT<int> bit(n);
-    repr(i, n) {
-        ans += bit.sum(v[i]);
+  private:
+    int _size;
+    std::vector<T> data;
+};
+/// @brief 転倒数
+template <class T>
+std::int64_t inversion_number(const std::vector<T> &v) {
+    if (v.empty()) return 0;
+    auto u = compress(v);
+    std::reverse(u.begin(), u.end());
+    fenwick_tree<T> bit(*std::max_element(u.begin(), u.end()) + 1);
+    std::int64_t res = 0;
+    for (auto x : u) {
+        res += bit.sum(x);
+        bit.add(x, 1);
+    }
+    return res;
+}
+/// @brief 転倒数
+template <class T>
+std::int64_t inversion_number_of_permutation(const std::vector<T> &v) {
+    if (v.empty()) return 0;
+    int n = v.size();
+    fenwick_tree<T> bit(n);
+    std::int64_t res = 0;
+    for (int i = n - 1; i >= 0; --i) {
+        res += bit.sum(v[i]);
         bit.add(v[i], 1);
     }
-    co(ans);
-
+    return res;
+}
+/// @brief 最小隣接スワップ回数
+template <class T>
+std::int64_t swap_distance(const std::vector<T> &a, const std::vector<T> &b) {
+    if (a.size() != b.size()) return -1;
+    int n = a.size();
+    std::vector<int> c(n), d(n);
+    std::iota(c.begin(), c.end(), 0);
+    std::iota(d.begin(), d.end(), 0);
+    std::stable_sort(c.begin(), c.end(), [&a](int x, int y) { return a[x] < a[y]; });
+    std::stable_sort(d.begin(), d.end(), [&b](int x, int y) { return b[x] < b[y]; });
+    std::vector<int> p(n);
+    for (int i = 0; i < n; ++i) {
+        int x = c[i], y = d[i];
+        if (a[x] != b[y]) return -1;
+        p[x] = y;
+    }
+    return inversion_number_of_permutation(p);
+}
+#ifdef ATCODER
+#pragma GCC target("sse4.2,avx512f,avx512dq,avx512ifma,avx512cd,avx512bw,avx512vl,bmi2")
+#endif
+#pragma GCC optimize("Ofast,fast-math,unroll-all-loops")
+#include <bits/stdc++.h>
+#ifndef ATCODER
+#pragma GCC target("sse4.2,avx2,bmi2")
+#endif
+template <class T, class U>
+constexpr bool chmax(T &a, const U &b) {
+    return a < (T)b ? a = (T)b, true : false;
+}
+template <class T, class U>
+constexpr bool chmin(T &a, const U &b) {
+    return (T)b < a ? a = (T)b, true : false;
+}
+constexpr std::int64_t INF = 1000000000000000003;
+constexpr int Inf = 1000000003;
+constexpr double EPS = 1e-7;
+constexpr double PI = 3.14159265358979323846;
+#define FOR(i, m, n) for (int i = (m); i < int(n); ++i)
+#define FORR(i, m, n) for (int i = (m)-1; i >= int(n); --i)
+#define FORL(i, m, n) for (int64_t i = (m); i < int64_t(n); ++i)
+#define rep(i, n) FOR (i, 0, n)
+#define repn(i, n) FOR (i, 1, n + 1)
+#define repr(i, n) FORR (i, n, 0)
+#define repnr(i, n) FORR (i, n + 1, 1)
+#define all(s) (s).begin(), (s).end()
+struct Sonic {
+    Sonic() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::fixed << std::setprecision(20);
+    }
+    constexpr void operator()() const {}
+} sonic;
+using namespace std;
+using ll = std::int64_t;
+using ld = long double;
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+    return is >> p.first >> p.second;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    for (T &i : v) is >> i;
+    return is;
+}
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    for (auto it = v.begin(); it != v.end(); ++it) os << (it == v.begin() ? "" : " ") << *it;
+    return os;
+}
+template <class Head, class... Tail>
+void co(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cout << head << '\n';
+    else std::cout << head << ' ', co(std::forward<Tail>(tail)...);
+}
+template <class Head, class... Tail>
+void ce(Head &&head, Tail &&...tail) {
+    if constexpr (sizeof...(tail) == 0) std::cerr << head << '\n';
+    else std::cerr << head << ' ', ce(std::forward<Tail>(tail)...);
+}
+void Yes(bool is_correct = true) { std::cout << (is_correct ? "Yes\n" : "No\n"); }
+void No(bool is_not_correct = true) { Yes(!is_not_correct); }
+void YES(bool is_correct = true) { std::cout << (is_correct ? "YES\n" : "NO\n"); }
+void NO(bool is_not_correct = true) { YES(!is_not_correct); }
+void Takahashi(bool is_correct = true) { std::cout << (is_correct ? "Takahashi" : "Aoki") << '\n'; }
+void Aoki(bool is_not_correct = true) { Takahashi(!is_not_correct); }
+int main(void) {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n), b(n);
+    for (auto &e : a) std::cin >> e;
+    for (auto &e : b) std::cin >> e;
+    for (int i = 0; i < n; ++i) {
+        a[i] += i, b[i] += i;
+    }
+    std::cout << swap_distance(a, b) << '\n';
     return 0;
 }
